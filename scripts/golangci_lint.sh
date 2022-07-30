3d9d69492dc6be48a46f5c7cc029eb8f3d79e247
Harjot Gill <harjot.gill@fluxninja.com>
2022-07-30 15:43:28 -0700

    update golangci-lint script

--- scripts/golangci_lint.sh
+++ scripts/golangci_lint.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 args=("${@}")
-readarray -t module_dirs < <(go list -f '{{.Dir}}' -m)
-parallel --bar --eta --no-notice --line-buffer --tag --colsep ' ' "golangci-lint run --color=always ${args[*]} {}/..." ::: "${module_dirs[@]}"
+golangci-lint run --color=always "${args[@]}" ./...
 code=$?
 
 exit $code
