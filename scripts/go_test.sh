adb6d651c04ac03439b2c86040acf3d8bf0769d7
Harjot Gill <harjot.gill@fluxninja.com>
2022-07-29 18:52:37 -0700

    add gotestsum

--- scripts/go_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-readarray -t module_dirs < <(go list -f '{{.Dir}}' -m)
-
-parallel --eta --no-notice --line-buffer --tag --colsep ' ' 'cd {} && go test ./...' ::: "${module_dirs[@]}"
