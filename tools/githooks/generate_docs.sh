99e49523b6b1840292965c41dd4b149104a462e5
Michał Krasnoborski <michal.krasnoborski@fluxninja.com>
2022-08-18 15:55:22 +0200

    Disable generate-mermaid in pre-commit (#127)

--- tools/githooks/generate_docs.sh
+++ tools/githooks/generate_docs.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 
 # generate docs
-make generate-docs
+# Note: this is not using generate-docs target, because of this bug:
+# https://github.com/fluxninja/aperture/issues/126
+# Regenerating svgs needs to be done manually via `make generate-mermaid`.
+make generate-config-markdown
