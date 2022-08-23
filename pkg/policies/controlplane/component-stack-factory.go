2c0965b7ba3c348fa22a80ac8f1a1da662762e43
Tanveer Gill <tanveer.gill@fluxninja.com>
2022-08-22 21:25:15 -0700

    Component Factory Cleanup (#150)

--- pkg/policies/controlplane/component-stack-factory.go
+++ pkg/policies/controlplane/component-stack-factory.go
@@ -21,41 +21,63 @@ func NewComponentStackAndOptions(
 	componentStackProto *policylangv1.Component,
 	componentStackIndex int,
 	policyReadAPI iface.PolicyRead,
-) (string, map[string]any, map[string]runtime.Component, fx.Option, error) {
+) (compiledComponent, []compiledComponent, fx.Option, error) {
 	// Factory parser to determine what kind of component stack to create
 	if concurrencyLimiter := componentStackProto.GetConcurrencyLimiter(); concurrencyLimiter != nil {
-		subComponents := make(map[string]runtime.Component)
-		var options []fx.Option
+		var (
+			compiledComponents []compiledComponent
+			options            []fx.Option
+		)
 		concurrencyLimiterOptions, agentGroupName, concurrencyLimiterErr := concurrency.NewConcurrencyLimiterOptions(concurrencyLimiter, componentStackIndex, policyReadAPI)
 		if concurrencyLimiterErr != nil {
-			return "", nil, nil, nil, concurrencyLimiterErr
+			return compiledComponent{}, nil, nil, concurrencyLimiterErr
 		}
+		// Append concurrencyLimiter options
 		options = append(options, concurrencyLimiterOptions)
 
 		// Scheduler
 		if schedulerProto := concurrencyLimiter.GetScheduler(); schedulerProto != nil {
-			subComponentKey := "scheduler"
 			scheduler, schedulerOptions, schedulerErr := concurrency.NewSchedulerAndOptions(schedulerProto, componentStackIndex, policyReadAPI, agentGroupName)
 			if schedulerErr != nil {
-				return "", nil, nil, nil, schedulerErr
+				return compiledComponent{}, nil, nil, schedulerErr
 			}
-			subComponents[subComponentKey] = scheduler
+			schedulerMapStruct, err := encodeMapStruct(schedulerProto)
+			if err != nil {
+				return compiledComponent{}, nil, nil, err
+			}
+			// Append scheduler as a compiledComponent
+			compiledComponents = append(compiledComponents, compiledComponent{
+				component: scheduler,
+				mapStruct: schedulerMapStruct,
+				name:      "Scheduler",
+			})
+
+			// Append scheduler options
 			options = append(options, schedulerOptions)
 		}
 
 		// Actuation Strategy
 		if loadShedActuatorProto := concurrencyLimiter.GetLoadShedActuator(); loadShedActuatorProto != nil {
-			subComponentKey := "load_shed_actuator"
 			loadShedActuator, loadShedActuatorOptions, loadShedActuatorErr := concurrency.NewLoadShedActuatorAndOptions(loadShedActuatorProto, componentStackIndex, policyReadAPI, agentGroupName)
 			if loadShedActuatorErr != nil {
-				return "", nil, nil, nil, loadShedActuatorErr
+				return compiledComponent{}, nil, nil, loadShedActuatorErr
 			}
-			subComponents[subComponentKey] = loadShedActuator
+			loadShedActuatorMapStruct, err := encodeMapStruct(loadShedActuatorProto)
+			if err != nil {
+				return compiledComponent{}, nil, nil, err
+			}
+			// Append loadShedActuator as a compiledComponent
+			compiledComponents = append(compiledComponents, compiledComponent{
+				component: loadShedActuator,
+				mapStruct: loadShedActuatorMapStruct,
+				name:      "LoadShedActuator",
+			})
+
+			// Append loadShedActuator options
 			options = append(options, loadShedActuatorOptions)
 		}
 
-		mapStruct, err := getComponentMapStruct(concurrencyLimiter, nil)
-		return "ConcurrencyLimiter", mapStruct, subComponents, fx.Options(options...), err
+		return compiledComponent{}, compiledComponents, fx.Options(options...), nil
 	}
-	return "", nil, nil, nil, fmt.Errorf("unsupported/missing component type")
+	return compiledComponent{}, nil, nil, fmt.Errorf("unsupported/missing component type")
 }
