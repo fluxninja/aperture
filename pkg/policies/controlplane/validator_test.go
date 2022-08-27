f3e525391b35794934a0d8d97e1b980c16bd4972
Harjot Gill <harjot.gill@fluxninja.com>
2022-08-27 16:03:10 -0700

    remove policy yaml files, please use jsonnet generator for testing

--- pkg/policies/controlplane/validator_test.go
+++ pkg/policies/controlplane/validator_test.go
@@ -2,9 +2,6 @@ package controlplane_test
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/ghodss/yaml"
 	. "github.com/onsi/ginkgo/v2"
@@ -31,17 +28,9 @@ var _ = Describe("Validator", func() {
 	cmVatidator := validation.NewCMValidator()
 	cmVatidator.RegisterCMFileValidator(cmFileValidator)
 
-	validateExample := func(name string) {
-		contents, err := os.ReadFile(filepath.Join(
-			"..", // policies
-			"..", // pkg
-			"..", // aperture
-			fmt.Sprintf("%s.yaml", name),
-		))
-		Expect(err).NotTo(HaveOccurred())
-
+	validateExample := func(contents string) {
 		var cm corev1.ConfigMap
-		err = yaml.Unmarshal(contents, &cm)
+		err := yaml.Unmarshal([]byte(contents), &cm)
 		Expect(err).NotTo(HaveOccurred())
 
 		ok, msg, err := cmVatidator.ValidateConfigMap(context.TODO(), cm)
@@ -51,6 +40,274 @@ var _ = Describe("Validator", func() {
 	}
 
 	It("accepts example configmap for demoapp", func() {
-		validateExample("latency-gradient-policy")
+		validateExample(latencyGradientPolicy)
+	})
+
+	It("accepts example configmap for classification", func() {
+		validateExample(classificationPolicy)
 	})
 })
+
+const latencyGradientPolicy = `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: policies
+  namespace: aperture-system
+  labels:
+    fluxninja.com/validate: "true"
+data:
+  latency-gradient.yaml: |
+    resources:
+      flux_meters:
+        "service_latency":
+          selector:
+            service: "demo1-demo-app.demoapp.svc.cluster.local"
+            control_point:
+              traffic: "ingress"
+      classifiers:
+        - selector:
+            service: demo1-demo-app.demoapp.svc.cluster.local
+            control_point: { traffic: ingress }
+          rules:
+            # An example rule using extractor.
+            # See following RFC for list of available extractors and their syntax.
+            ua:
+              extractor:
+                from: request.http.headers.user-agent
+            # The same rule using raw rego. Requires specifying rego source code and a query
+            also-ua:
+              rego:
+                source: |
+                  package my.rego.pkg
+                  import input.attributes.request.http
+                  ua = http.headers["user-agent"]
+                query: data.my.rego.pkg.ua
+            user-type:
+              extractor:
+                from: request.http.headers.user-type
+    circuit:
+      evaluation_interval: "0.5s"
+      components:
+        - promql:
+            query_string: "sum(increase(flux_meter_sum{decision_type!=\"DECISION_TYPE_REJECTED\", policy_name=\"latency-gradient\", flux_meter_name=\"service_latency\"}[5s]))/sum(increase(flux_meter_count{decision_type!=\"DECISION_TYPE_REJECTED\", policy_name=\"latency-gradient\", flux_meter_name=\"service_latency\"}[5s]))"
+            evaluation_interval: "1s"
+            out_ports:
+              output:
+                signal_name: "LATENCY"
+        - constant:
+            value: "2.0"
+            out_ports:
+              output:
+                signal_name: "EMA_LIMIT_MULTIPLIER"
+        - arithmetic_combinator:
+            operator: "mul"
+            in_ports:
+              lhs:
+                signal_name: "LATENCY"
+              rhs:
+                signal_name: "EMA_LIMIT_MULTIPLIER"
+            out_ports:
+              output:
+                signal_name: "MAX_EMA"
+        - ema:
+            ema_window: "300s"
+            warm_up_window: "10s"
+            correction_factor_on_max_envelope_violation: "0.95"
+            in_ports:
+              input:
+                signal_name: "LATENCY"
+              max_envelope:
+                signal_name: "MAX_EMA"
+            out_ports:
+              output:
+                signal_name: "LATENCY_EMA"
+        - gradient_controller:
+            tolerance: "1.1"
+            min_gradient: "0.1"
+            max_gradient: "1.0"
+            in_ports:
+              signal:
+                signal_name: "LATENCY"
+              setpoint:
+                signal_name: "LATENCY_EMA"
+              max:
+                signal_name: "MAX_CONCURRENCY"
+              control_variable:
+                signal_name: "ACCEPTED_CONCURRENCY"
+              optimize:
+                signal_name: "CONCURRENCY_INCREMENT"
+            out_ports:
+              output:
+                signal_name: "DESIRED_CONCURRENCY"
+        - arithmetic_combinator:
+            operator: "sub"
+            in_ports:
+              lhs:
+                signal_name: "INCOMING_CONCURRENCY"
+              rhs:
+                signal_name: "DESIRED_CONCURRENCY"
+            out_ports:
+              output:
+                signal_name: "DELTA_CONCURRENCY"
+        - arithmetic_combinator:
+            operator: "div"
+            in_ports:
+              lhs:
+                signal_name: "DELTA_CONCURRENCY"
+              rhs:
+                signal_name: "INCOMING_CONCURRENCY"
+            out_ports:
+              output:
+                signal_name: "LSF"
+        - concurrency_limiter:
+            scheduler:
+              selector:
+                service: "demo1-demo-app.demoapp.svc.cluster.local"
+                control_point:
+                  traffic: "ingress"
+              auto_tokens: true
+              default_workload:
+                priority: 20
+                timeout: "0.005s"
+              workloads:
+                - workload:
+                    priority: 50
+                    timeout: "0.005s"
+                  label_matcher:
+                    match_labels:
+                      user-type: "guest"
+                - workload:
+                    priority: 200
+                    timeout: "0.005s"
+                  label_matcher:
+                    match_labels:
+                      request_header_user-type: "subscriber"
+              out_ports:
+                accepted_concurrency:
+                  signal_name: "ACCEPTED_CONCURRENCY"
+                incoming_concurrency:
+                  signal_name: "INCOMING_CONCURRENCY"
+            load_shed_actuator:
+              in_ports:
+                load_shed_factor:
+                  signal_name: "LSF"
+        - constant:
+            value: "2.0"
+            out_ports:
+              output:
+                signal_name: "CONCURRENCY_LIMIT_MULTIPLIER"
+        - constant:
+            value: "10.0"
+            out_ports:
+              output:
+                signal_name: "MIN_CONCURRENCY"
+        - constant:
+            value: "5.0"
+            out_ports:
+              output:
+                signal_name: "LINEAR_CONCURRENCY_INCREMENT"
+        - arithmetic_combinator:
+            operator: "mul"
+            in_ports:
+              lhs:
+                signal_name: "CONCURRENCY_LIMIT_MULTIPLIER"
+              rhs:
+                signal_name: "ACCEPTED_CONCURRENCY"
+            out_ports:
+              output:
+                signal_name: "UPPER_CONCURRENCY_LIMIT"
+        - max:
+            in_ports:
+              inputs:
+                - signal_name: "UPPER_CONCURRENCY_LIMIT"
+                - signal_name: "MIN_CONCURRENCY"
+            out_ports:
+              output:
+                signal_name: "MAX_CONCURRENCY"
+        - sqrt:
+            scale: "0.5"
+            in_ports:
+              input:
+                signal_name: "ACCEPTED_CONCURRENCY"
+            out_ports:
+              output:
+                signal_name: "SQRT_CONCURRENCY_INCREMENT"
+        - arithmetic_combinator:
+            operator: "add"
+            in_ports:
+              lhs:
+                signal_name: "LINEAR_CONCURRENCY_INCREMENT"
+              rhs:
+                signal_name: "SQRT_CONCURRENCY_INCREMENT"
+            out_ports:
+              output:
+                signal_name: "CONCURRENCY_INCREMENT_NORMAL"
+        - constant:
+            value: "1.2"
+            out_ports:
+              output:
+                signal_name: "OVERLOAD_MULTIPLIER"
+        - arithmetic_combinator:
+            operator: "mul"
+            in_ports:
+              lhs:
+                signal_name: "LATENCY_EMA"
+              rhs:
+                signal_name: "OVERLOAD_MULTIPLIER"
+            out_ports:
+              output:
+                signal_name: "LATENCY_OVERLOAD"
+        - constant:
+            value: "10.0"
+            out_ports:
+              output:
+                signal_name: "CONCURRENCY_INCREMENT_OVERLOAD"
+        - decider:
+            operator: "gt"
+            in_ports:
+              lhs:
+                signal_name: "LATENCY"
+              rhs:
+                signal_name: "LATENCY_OVERLOAD"
+              on_true:
+                signal_name: "CONCURRENCY_INCREMENT_OVERLOAD"
+              on_false:
+                signal_name: "CONCURRENCY_INCREMENT_NORMAL"
+            out_ports:
+              output:
+                signal_name: "CONCURRENCY_INCREMENT"
+  `
+
+const classificationPolicy = `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: policies
+  namespace: aperture-system
+  labels:
+    fluxninja.com/validate: "true"
+data:
+  productpage.yaml: |
+    resources:
+      classifiers:
+        - selector:
+            service: productpage.bookinfo.svc.cluster.local
+            control_point: { traffic: ingress }
+          rules:
+            ua:
+              extractor:
+                from: request.http.headers.user-agent
+            user:
+              rego:
+                query: data.user_from_cookie.user
+                source: |
+                  package user_from_cookie
+                  cookies := split(input.attributes.request.http.headers.cookie, "; ")
+                  cookie := cookies[_]
+                  cookie.startswith("session=")
+                  session := substring(cookie, count("session="), -1)
+                  parts := split(session, ".")
+                  object := json.unmarshal(base64url.decode(parts[0]))
+                  user := object.user
+`
