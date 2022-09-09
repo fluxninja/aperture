9c83dd399e46afa4ca04e689b24e7ddf2a62de7b
Harjot Gill <harjot.gill@fluxninja.com>
2022-09-08 21:43:37 -0700

    tweaks

--- tools/load_generator/scenarios/load_test.js
+++ tools/load_generator/scenarios/load_test.js
@@ -44,7 +44,7 @@ export default function () {
     "http status was 200": res.status === 200,
   });
   if (!ret) {
-    // sleep for 25ms to 50ms
-    sleep(randomIntBetween(0.25, 0.5));
+    // sleep for 5ms to 25ms
+    sleep(randomIntBetween(0.005, 0.025));
   }
 }
