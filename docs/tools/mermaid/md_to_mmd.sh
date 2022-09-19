bee427b757c8f351d4a696efc3615f5f0150f728
Harjot Gill <harjot.gill@fluxninja.com>
2022-09-18 18:13:21 -0700

    fix mermaid generator on macOS (#510)

--- docs/tools/mermaid/md_to_mmd.sh
+++ docs/tools/mermaid/md_to_mmd.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+GREP="grep"
+SED="sed"
+AWK="awk"
+
+# check whether we are using macOS
+if [ "$(uname)" == "Darwin" ]; then
+	GREP="ggrep"
+	SED="gsed"
+	AWK="gawk"
+fi
+
 # script that parses all markdown (md) files recursively in a directory
 # and extracts content for the mermaid sections that begin with ```mermaid and end with ```
 # and generates mmd files for each mermaid section
@@ -11,16 +22,17 @@ for d in $dirs; do
 	for f in $files; do
 		# extract all mermaid multiline sections that start with ```mermaid and end with ``` into an array of sections
 		#shellcheck disable=SC2002,SC2016
-		cat "$f" | sed -n '/```mermaid/,/```/p' | grep -vP '^```$' >records.txt
+		cat "$f" | $SED -n '/```mermaid/,/```/p' | $GREP -vP '^```$' >records.txt
 		# use awk to separate out mermaid_records using RS='```' into an array of sections
-		awk '{RS="```mermaid"} NR > 1 { print $0 > "mermaid_section_" ++i}' records.txt
+		#shellcheck disable=SC2016
+		$AWK '{RS="```mermaid"} NR > 1 { print $0 > "mermaid_section_" ++i}' records.txt
 		# for each mermaid section, generate a mmd file
 		mermaid_section_files=$(find . -type f -name "mermaid_section_*")
 		count=0
 		for mermaid_section_file in $mermaid_section_files; do
 			# search for name in the comment - "%% name: <name>"
 			# if found, use the name as the mmd file name
-			name=$(grep -P '^%% name: ' "$mermaid_section_file" | sed -e 's/%% name: //')
+			name=$($GREP -P '^%% name: ' "$mermaid_section_file" | $SED -e 's/%% name: //')
 			if [ -n "$name" ]; then
 				outfilename="$name.mmd"
 			else
