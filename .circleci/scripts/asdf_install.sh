cfdfe1c3a19577a49d1862b85dbe4deafe474029
Harjot Gill <harjot.gill@fluxninja.com>
2022-09-22 17:32:57 -0700

    install graphviz

--- .circleci/scripts/asdf_install.sh
+++ .circleci/scripts/asdf_install.sh
@@ -15,7 +15,8 @@ if grep <<<"${TOOLS}" '^python$'; then
 	sudo apt-get update
 	sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 		libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-		libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+		libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+		graphviz
 fi
 
 (yes || true) | ./playground/scripts/manage_tools.sh setup "${tools[@]}"
