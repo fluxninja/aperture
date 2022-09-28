b40e9df2d06f3c375d10b666462a31f86418f2fe
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2022-09-28 12:03:39 +0200

    Populate version in build

--- pkg/info/build.sh
+++ pkg/info/build.sh
@@ -4,6 +4,7 @@ set -eux
 # This script builds a Go binary and injects build-time variables.
 # TODO: it should be used in every Dockerfile requiring version endpoint.
 
+VERSION=${VERSION:-0.0.1}
 BUILD_TIME=$(date --rfc-3339=seconds)
 GOOS=$(go env GOOS)
 GOARCH=$(go env GOARCH)
@@ -13,6 +14,7 @@ SERVICE="${SERVICE_FILE%.*}"
 
 LDFLAGS="\
     ${LDFLAGS:-} \
+    -X 'github.com/fluxninja/aperture/pkg/info.Version=${VERSION}' \
     -X 'github.com/fluxninja/aperture/pkg/info.Service=${SERVICE}' \
     -X 'github.com/fluxninja/aperture/pkg/info.BuildHost=${HOSTNAME}' \
     -X 'github.com/fluxninja/aperture/pkg/info.BuildOS=${GOOS}/${GOARCH}' \
