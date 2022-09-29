e93c619710f8fe855619932746c98f0553a07b1f
Harjot Gill <harjot.gill@fluxninja.com>
2022-09-29 09:27:15 -0700

    fix test

--- pkg/webhooks/policyvalidator/validator.go
+++ pkg/webhooks/policyvalidator/validator.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/fluxninja/aperture/operator/api"
 	policyv1alpha1 "github.com/fluxninja/aperture/operator/api/policy/v1alpha1"
-	"github.com/fluxninja/aperture/pkg/log"
 )
 
 // limit the concurrent validation requests (some validations incurs rego
@@ -79,7 +78,6 @@ func (v *PolicyValidator) ValidateObject(
 	if err != nil {
 		return false, "Not a valid Policy Object", err
 	}
-	log.Info().Bytes("PolicySpec", policy.Spec.Raw).Msg("Validating Policy Spec")
 
 	for _, validator := range v.policyValidators {
 		ok, msg, err := validator.ValidateSpec(ctx, policy.GetName(), policy.Spec.Raw)
