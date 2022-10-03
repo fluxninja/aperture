dd0da98f29877d6fe255a5e186031c4880841895
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2022-10-03 15:46:19 +0200

    Populate version in build (#614)
    * Populate version in build
    
    * Added CircleCi config to pass the version
    
    * Fixed docker build args issue
    
    * Corrected indentation
    
    Co-authored-by: Hardik Shingala <hardik.shingala@fluxninja.com>

--- .circleci/scripts/docker_build.sh
+++ .circleci/scripts/docker_build.sh
@@ -33,6 +33,22 @@ pull_images_from_cache() {
     done
 }
 
+fetch_aperture_version() {
+    component=aperture-controller
+    tag_matcher="releases/${component}/*"
+    current_branch="$(git branch --show-current)"
+
+    if [[ "${current_branch}" == "stable/"* ]]; then
+        version="$(git describe --match "${tag_matcher}" | cut -d/ -f 3)"
+    else
+        tag="$(git tag -l --sort="-version:refname" "${tag_matcher}" | head -n1 | cut -d/ -f 3)"
+        commits="$(git rev-list "${tag}"..HEAD --count)"
+        version="${tag##*/}-b.${commits}"
+    fi
+
+    echo "${version}" | cut -dv -f 2
+}
+
 if ! parse_tags_to_docker_arg; then
     echo "Unable to parse provided tags."
     echo "Check your \"tag\" parameter or refer to the docs and try again: https://circleci.com/developer/orbs/orb/circleci/docker."
@@ -50,8 +66,9 @@ fi
 build_args=(
     "--file=$PARAM_DOCKERFILE_PATH/$PARAM_DOCKERFILE_NAME"
     "$DOCKER_TAGS_ARG"
-    "--build-arg=$PARAM_GIT_BRANCH"
-    "--build-arg=$PARAM_GIT_COMMIT_HASH"
+    "--build-arg=GIT_BRANCH=$PARAM_GIT_BRANCH"
+    "--build-arg=GIT_COMMIT_HASH=$PARAM_GIT_COMMIT_HASH"
+    "--build-arg=VERSION=$(fetch_aperture_version)"
 )
 
 if [ -n "$PARAM_SSH_FORWARD" ]; then
