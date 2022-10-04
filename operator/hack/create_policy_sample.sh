9343f2c654cb28f59739d61edd6c2475ae7dad39
Harjot Gill <harjot.gill@fluxninja.com>
2022-10-04 15:00:55 -0700

    copy example policy to crd samples

--- operator/hack/create_policy_sample.sh
+++ operator/hack/create_policy_sample.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# find git root
+pushd "$(git rev-parse --show-toplevel)" >/dev/null
+
 CR_PREAMBLE_YAML=$(
 	cat <<-END
 		apiVersion: fluxninja.com/v1alpha1
@@ -12,3 +15,5 @@ CR_PREAMBLE_YAML=$(
 
 # append contents of blueprints/blueprints/latency-gradient/example/gen/policies/example.yaml to CR_PREAMBLE_YAML in the next line under spec key
 yq eval-all 'select(fileIndex == 0) * {"spec": select(fileIndex == 1)}' <(echo "$CR_PREAMBLE_YAML") blueprints/blueprints/latency-gradient/example/gen/policies/example.yaml >operator/config/samples/fluxninja.com_v1alpha1_policy.yaml
+
+popd >/dev/null
