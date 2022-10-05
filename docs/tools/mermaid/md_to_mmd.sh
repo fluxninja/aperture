5de8abe3e512fbbcad85258da7d084f777408dd9
Harjot Gill <harjot.gill@fluxninja.com>
2022-10-05 13:16:04 -0700

    Refactor mermaid generator (#691)

--- docs/tools/mermaid/md_to_mmd.sh
+++ docs/tools/mermaid/md_to_mmd.sh
@@ -17,18 +17,24 @@ fi
 # script that parses all markdown (md) files recursively in a directory
 # and extracts content for the mermaid sections that begin with ```mermaid and end with ```
 # and generates mmd files for each mermaid section
-out_dir="$docsdir"/content/assets/gen
-rm -f "$out_dir"/*.mmd || true
+rm -rf tmp
+mkdir -p tmp
 files=$(find "$docsdir"/content -type f -name "*.md")
 for f in $files; do
+	dir=$(dirname "$f")
+	filename=$(basename "$f")
+	filenameNoExt="${filename%.*}"
+	out_dir="$dir"/assets/gen/"$filenameNoExt"
+	mkdir -p "$out_dir"
+	rm -f "$out_dir"/*.mmd
 	# extract all mermaid multiline sections that start with ```mermaid and end with ``` into an array of sections
 	#shellcheck disable=SC2002,SC2016
-	cat "$f" | $SED -n '/```mermaid/,/```/p' | $GREP -vP '^```$' >records.txt
+	cat "$f" | $SED -n '/```mermaid/,/```/p' | $GREP -vP '^```$' >tmp/records.txt
 	# use awk to separate out mermaid_records using RS='```' into an array of sections
 	#shellcheck disable=SC2016
-	$AWK '{RS="```mermaid"} NR > 1 { print $0 > "mermaid_section_" ++i}' records.txt
+	$AWK '{RS="```mermaid"} NR > 1 { print $0 > "tmp/mermaid_section_" ++i}' tmp/records.txt
 	# for each mermaid section, generate a mmd file
-	mermaid_section_files=$(find . -type f -name "mermaid_section_*")
+	mermaid_section_files=$(find tmp -type f -name "mermaid_section_*")
 	count=0
 	for mermaid_section_file in $mermaid_section_files; do
 		# search for name in the comment - "%% name: <name>"
@@ -42,10 +48,11 @@ for f in $files; do
 		# generate mmd
 		echo "generating $outfilename"
 		mv "$mermaid_section_file" "$out_dir"/"$outfilename"
+		git add "$out_dir"/"$outfilename"
 		# increment count
 		count=$((count + 1))
 	done
-	rm -f records.txt || true
-	rm -f mermaid_section_* || true
+	rm -rf tmp/*
 done
-git add "$out_dir"/*.mmd
+
+rm -rf tmp
