096155ac3cca4e5196aa856a6baea4faa044f952
Harjot Gill <harjot.gill@fluxninja.com>
2022-10-04 17:18:35 -0700

    validate jsonnet examples

--- scripts/go_build.sh
+++ scripts/go_build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+gitroot=$(git rev-parse --show-toplevel)
+
+pushd "$gitroot" >/dev/null || exit 1
+
 # list all directories that contain .go files except for vendor directories
 dirs=$(grep --include="*.go" --exclude-dir="vendor" --exclude-dir="plugins" -r "package main" -l | xargs dirname | sort -u)
 
 # use parallel to execute "cd {} && go build" in for each directory in $dirs
 parallel -j4 --no-notice --bar --eta "cd {} && go build" ::: "$dirs"
+
+popd >/dev/null || exit 1
