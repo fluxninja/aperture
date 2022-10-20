690f6ec5fd3414c268fda17cd8fd3bb628dbf2b2
Hasit Mistry <hasit@fluxninja.com>
2022-10-20 10:25:21 -0700

    Update CommonHandler

--- cmd/sdk-validator/validator/common.go
+++ cmd/sdk-validator/validator/common.go
@@ -17,13 +17,11 @@ type CommonHandler struct {
 
 // CheckWithValues is a dummy function for creating *flowcontrolv1.CheckResponse from given parameters.
 func (c *CommonHandler) CheckWithValues(ctx context.Context, services []string, controlPoint selectors.ControlPoint, labels map[string]string) *flowcontrolv1.CheckResponse {
-	resp := &flowcontrolv1.CheckResponse{
+	return &flowcontrolv1.CheckResponse{
 		DecisionType:     flowcontrolv1.CheckResponse_DECISION_TYPE_ACCEPTED,
 		FlowLabelKeys:    maps.Keys(labels),
 		Services:         services,
 		ControlPointInfo: controlPoint.ToControlPointInfoProto(),
 		RejectReason:     flowcontrolv1.CheckResponse_REJECT_REASON_NONE,
 	}
-
-	return resp
 }
