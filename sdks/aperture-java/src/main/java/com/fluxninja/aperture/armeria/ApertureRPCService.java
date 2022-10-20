9a31653d3560a0e191991e34cbb3a2ec1206661c
Filip Chmielewski <filip.chmielewski@fluxninja.com>
2022-10-20 03:17:03 +0200

    Use envoy authz in java sdk

--- sdks/aperture-java/src/main/java/com/fluxninja/aperture/armeria/ApertureRPCService.java
+++ sdks/aperture-java/src/main/java/com/fluxninja/aperture/armeria/ApertureRPCService.java
@@ -2,7 +2,7 @@ package com.fluxninja.aperture.armeria;
 
 import com.fluxninja.aperture.sdk.ApertureSDK;
 import com.fluxninja.aperture.sdk.ApertureSDKException;
-import com.fluxninja.aperture.sdk.Flow;
+import com.fluxninja.aperture.sdk.FeatureFlow;
 import com.fluxninja.aperture.sdk.FlowStatus;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.RpcRequest;
@@ -34,7 +34,7 @@ public class ApertureRPCService extends SimpleDecoratingRpcService {
     @Override
     public RpcResponse serve(ServiceRequestContext ctx, RpcRequest req) throws Exception {
         Map<String, String> labels = RpcUtils.labelsFromRequest(req);
-        Flow flow = this.apertureSDK.startFlow("", labels);
+        FeatureFlow flow = this.apertureSDK.startFlow("", labels);
 
         if (flow.accepted()) {
             RpcResponse res;
