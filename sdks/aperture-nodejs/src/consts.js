1f8b254afe4731388c88f289db3b33fc67f3da36
Daria Danilenko <daria.danilenko@fluxninja.com>
2022-10-24 20:06:13 +0200

    Nodejs SDK simple grpc client

---
+++ sdks/aperture-nodejs/src/consts.js
@@ -0,0 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+export const PROTO_PATH = path.resolve(
+  path.dirname(__filename),
+  "./proto/flowcontrol.proto"
+);
+
+export const host = 'localhost';
+export const port = process.env.FN_APP_PORT ? process.env.FN_APP_PORT : "8000";
+
+const fn_host = process.env.FN_AGENT_HOST ? process.env.FN_AGENT_HOST : "localhost";
+const fn_port = process.env.FN_AGENT_PORT ? process.env.FN_AGENT_PORT : "8089";
+export const URL = fn_host + ":" + fn_port;
