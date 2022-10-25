edd66a660f97185370ec3e543ba14dc95d6d49b5
Daria Danilenko <daria.danilenko@fluxninja.com>
2022-10-25 21:16:14 +0200

    Repo rename, add circle ci sync job

--- sdks/aperture-js/src/example.js
+++ sdks/aperture-js/src/example.js
@@ -6,7 +6,7 @@ import { host, port } from "./consts.js";
 const requestListener = function (req, res) {
   console.log(`Got a request`);
   var labelsMap = new Map().set('labelKey','labelValue');
-  client.StartFlow("aperture-nodejs", labelsMap).then(() => {
+  client.StartFlow("aperture-js", labelsMap).then(() => {
     console.log('StartFlow Done');
     res.writeHead(200);
     res.end('Hello, World!\n');
