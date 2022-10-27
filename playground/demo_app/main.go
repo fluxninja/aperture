961bbad296a20f1980fbeb4e6aea5299ff61f149
Hasit Mistry <hasit@fluxninja.com>
2022-10-26 17:42:58 -0700

    Fix import in demo_app

--- playground/demo_app/main.go
+++ playground/demo_app/main.go
@@ -14,7 +14,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 
 	"github.com/fluxninja/aperture/pkg/log"
-	"github.com/fluxninja/aperture/tools/demo_app/app"
+	"github.com/fluxninja/aperture/playground/demo_app/app"
 )
 
 func main() {
