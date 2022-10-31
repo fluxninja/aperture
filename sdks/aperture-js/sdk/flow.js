98f5270d6eff11c2457074571c43b41f271e08fc
Daria Danilenko <daria.danilenko@fluxninja.com>
2022-10-31 14:53:29 +0100

    Cleanup statuses

--- sdks/aperture-js/sdk/flow.js
+++ sdks/aperture-js/sdk/flow.js
@@ -1,3 +1,22 @@
+export const FlowStatus = Object.freeze({
+	Ok: Symbol(0),
+	Error: Symbol(1)
+})
+
 export class Flow {
-    
+    constructor(checkResponse) {
+        this.checkResponse = checkResponse;
+    }
+
+    Accepted() {
+        return true;
+    }
+
+	End(flowStatus) {
+        return true;
+    }
+
+	CheckResponse() {
+        return this.checkResponse;
+    }
 }
