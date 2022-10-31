7c805c02e94f2989e8ae8460996f329d6bfdbbc4
Daria Danilenko <daria.danilenko@fluxninja.com>
2022-10-31 14:53:29 +0100

    Cleanup statuses

--- sdks/aperture-js/routes/use_aperture.js
+++ sdks/aperture-js/routes/use_aperture.js
@@ -1,27 +1,37 @@
 import express from 'express';
 import { ApertureClient } from "../sdk/client.js";
-
-export const apertureRoute = express.Router();
+import { FlowStatus } from "../sdk/flow.js";
 
 // Create aperture client
 export const apertureClient = new ApertureClient();
 
+export const apertureRoute = express.Router();
 apertureRoute.get('/', function (req, res) {
-    console.log(`Got a request`);
-
     // do some business logic to collect labels
-    var labelsMap = new Map().set('labelKey','labelValue');
+    var labelsMap = new Map().set('user', 'kenobi');
 
     // StartFlow performs a flowcontrolv1.Check call to Aperture Agent. It returns a Flow and an error if any.
-    apertureClient.StartFlow("aperture-js", labelsMap).then((response) => {
-        console.log('StartFlow Done');
-        res.writeHead(200);
-        res.end('Hello, World!\n');
-    }).then( (rejection) => {
-        console.log(`StartFlow failed: ${rejection}`);
-        res.status(200).send("StartFlow failed\n");
+    apertureClient.StartFlow("aperture-js", labelsMap).then((flow) => {
+        // See whether flow was accepted by Aperture Agent.
+        if (flow.Accepted()) {
+            // Simulate work being done
+            sleep(2000).then(() => { console.log("Work done!\n"); });
+
+            // Need to call End() on the Flow in order to provide telemetry to Aperture Agent for completing the control loop.
+            // The first argument captures whether the feature captured by the Flow was successful or resulted in an error.
+            flow.End(FlowStatus.Ok);
+            res.sendStatus(200);
+        } else {
+            // Flow has been rejected by Aperture Agent.
+            flow.End(FlowStatus.Error);
+            res.sendStatus(403);
+        }
     }).catch(e => {
         console.log(e);
         res.send(`Error ocurred: ${e}\n`);
     });
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
