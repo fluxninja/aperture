f4418096d34a8a27d4d0f16e787006410e1a3982
Daria Danilenko <daria.danilenko@fluxninja.com>
2022-11-02 23:17:20 +0100

    Add telemetry to NodeJS SDK

--- sdks/aperture-js/sdk/client.js
+++ sdks/aperture-js/sdk/client.js
@@ -1,48 +1,94 @@
 import grpc from "@grpc/grpc-js";
-import protoLoader from "@grpc/proto-loader";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 
-import { PROTO_PATH, URL } from "./consts.js";
+import { fcs } from "./utils.js";
+import {
+  URL,
+  LIBRARY_NAME,
+  LIBRARY_VERSION,
+  FLOW_START_TIMESTAMP_LABEL,
+  SOURCE_LABEL,
+  WORKLOAD_START_TIMESTAMP_LABEL,
+} from "./consts.js";
 import { Flow } from "./flow.js";
 
-const clientPackage = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
 
-const fcs = grpc.loadPackageDefinition(clientPackage).aperture.flowcontrol.v1;
 
 export class ApertureClient {
-  constructor() {
+  constructor(timeout = 200) {
     this.fcsClient = new fcs.FlowControlService(URL, grpc.credentials.createInsecure());
+
+    let exporter = new OTLPTraceExporter({
+      url: URL,
+      credentials: grpc.credentials.createInsecure(),
+    });
+    let res = this.#newResource();
+    let tracerProvider = new NodeTracerProvider({
+      resource: res,
+    });
+    tracerProvider.addSpanProcessor(new BatchSpanProcessor(exporter));
+    tracerProvider.register();
+    this.tracer = tracerProvider.getTracer(LIBRARY_NAME, LIBRARY_VERSION);
   }
 
+  // StartFlow takes a feature name and labels that get passed to Aperture Agent via flowcontrolv1.Check call.
+  // Return value is a Flow.
+  // The call returns immediately in case connection with Aperture Agent is not established.
+  // The default semantics are fail-to-wire. If StartFlow fails, calling Flow.Accepted() on returned Flow returns as true.
   async StartFlow(featureArg, labelsArg) {
     return new Promise((resolve, reject) => {
+      // TODO - process baggage
+
+      let span = this.tracer.startSpan("Aperture Check");
+      span.setAttributes({
+        FLOW_START_TIMESTAMP_LABEL: Date.now(),
+        SOURCE_LABEL: "sdk",
+      });
+      let flow = new Flow(span);
+
       this.fcsClient.Check(
         {
           feature: featureArg,
           labels: labelsArg,
         }, (err, response) => {
+          span.setAttribute(WORKLOAD_START_TIMESTAMP_LABEL, Date.now());
+
           if (err) {
+            if (err.code === grpc.status.UNAVAILABLE) {
+              console.log(`Aperture server unavailable. Accepting request.\n`);
+              flow.checkResponse = response;
+              resolve(flow);
+            }
             reject(err);
           }
 
           console.log(`Response: ${response}\n`);
-          let flow = new Flow(response);
+          flow.checkResponse = response;
           resolve(flow);
         });
     });
   }
 
   Shutdown() {
-    // TODO
+    grpc.closeClient(this.fcsClient);
     return;
   }
 
   GetState() {
     return this.fcsClient.getChannel().getConnectivityState(true);
+  }
+
+  #newResource() {
+    let defaultRes = Resource.default();
+    let res = new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: LIBRARY_NAME,
+      [SemanticResourceAttributes.SERVICE_VERSION]: LIBRARY_VERSION,
+    });
+    let merged = defaultRes.merge(res);
+    return merged;
   }
 }
--- sdks/aperture-js/sdk/flow.js
+++ sdks/aperture-js/sdk/flow.js
@@ -1,19 +1,44 @@
+import { fcs } from "./utils.js";
+import {
+    FEATURE_STATUS_LABEL,
+    CHECK_RESPONSE_LABEL,
+    FLOW_END_TIMESTAMP_LABEL,
+} from "./consts.js";
+
 export const FlowStatus = Object.freeze({
     Ok: Symbol(0),
     Error: Symbol(1)
 })
 
 export class Flow {
-    constructor(checkResponse) {
+    constructor(span, checkResponse = null) {
+        this.span = span;
+        this.ended = false;
         this.checkResponse = checkResponse;
     }
 
     Accepted() {
-        return true;
+        if (this.checkResponse === undefined) {
+            return true;
+        }
+        if (this.checkResponse.DecisionType === fcs.CheckResponse_DECISION_TYPE_ACCEPTED) {
+            return true;
+        }
+        return false;
     }
 
     End(flowStatus) {
-        return true;
+        if (this.ended) {
+            return new Error("flow already ended");
+        }
+        this.ended = true;
+
+        this.span.setAttributes({
+            FEATURE_STATUS_LABEL: flowStatus,
+            CHECK_RESPONSE_LABEL: this.checkResponse,
+            FLOW_END_TIMESTAMP_LABEL: Date.now(),
+        });
+        this.span.end();
     }
 
     CheckResponse() {
