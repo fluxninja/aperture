a7d0493f802c96bae8bebfa6b15a59d67c4e16c0
Seunghyup Alex Oh <seuoh@fluxninja.com>
2022-11-21 09:03:43 -0800

    Fix import issue

--- pkg/watchdog/watchdog.go
+++ pkg/watchdog/watchdog.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/elastic/gosigar"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/rs/zerolog"
 	"go.uber.org/fx"
 	"go.uber.org/multierr"
 	"google.golang.org/protobuf/proto"
