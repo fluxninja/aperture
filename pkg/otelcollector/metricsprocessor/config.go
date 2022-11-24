915d905214afe521e1ea2e75d7863b377fea45d6
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2022-11-24 16:55:48 +0100

    Add control points handling

--- pkg/otelcollector/metricsprocessor/config.go
+++ pkg/otelcollector/metricsprocessor/config.go
@@ -4,6 +4,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"go.opentelemetry.io/collector/config"
 
+	"github.com/fluxninja/aperture/pkg/controlpointcache"
 	"github.com/fluxninja/aperture/pkg/policies/flowcontrol/iface"
 )
 
@@ -12,5 +13,6 @@ type Config struct {
 	promRegistry             *prometheus.Registry
 	engine                   iface.Engine
 	classificationEngine     iface.ClassificationEngine
+	controlPointCache        *controlpointcache.ControlPointCache
 	config.ProcessorSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct
 }
