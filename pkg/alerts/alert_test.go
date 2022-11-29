bd23bde06deb3f939b38119385c7dc3966993f1f
Daria Danilenko <daria.danilenko@fluxninja.com>
2022-11-30 00:28:26 +0100

    Fix tests

--- pkg/alerts/alert_test.go
+++ pkg/alerts/alert_test.go
@@ -1,10 +1,8 @@
 package alerts_test
 
 import (
-	"github.com/go-openapi/strfmt"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/prometheus/alertmanager/api/v2/models"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 
 	"github.com/fluxninja/aperture/pkg/alerts"
@@ -12,7 +10,12 @@ import (
 
 var _ = Describe("Alert", func() {
 	It("transforms both ways properly", func() {
-		alert := exampleAlert()
+		alert := alerts.NewAlert(
+			alerts.WithAnnotation("one", "eleven"),
+			alerts.WithAnnotation("two", "twelve"),
+			alerts.WithName("buzz"),
+			alerts.WithSeverity("error"),
+		)
 		ld := alert.AsLogs()
 		Expect(ld.LogRecordCount()).To(Equal(1))
 		actual := alerts.AlertsFromLogs(ld)
@@ -25,25 +28,4 @@ func expectKeyWithValue(attributes pcommon.Map, key, value string) {
 	rawValue, exists := attributes.Get(key)
 	Expect(exists).To(BeTrue())
 	Expect(rawValue.AsString()).To(Equal(value))
-}
-
-func exampleAlert() *alerts.Alert {
-	startsAt, err := strfmt.ParseDateTime("2022-10-26T07:44:14.101Z")
-	Expect(err).NotTo(HaveOccurred())
-	return &alerts.Alert{
-		models.PostableAlert{
-			StartsAt: startsAt,
-			Annotations: map[string]string{
-				"one": "eleven",
-				"two": "twelve",
-			},
-			Alert: models.Alert{
-				GeneratorURL: strfmt.URI("https://www.example.org/alertSource"),
-				Labels: map[string]string{
-					"alertname": "buzz",
-					"severity":  "error",
-				},
-			},
-		},
-	}
 }
