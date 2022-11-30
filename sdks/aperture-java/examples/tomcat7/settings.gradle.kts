0cfd25240cc7bf08c661fdcc9d54d09b6d0d9737
Filip Chmielewski <filip.chmielewski@fluxninja.com>
2022-11-30 09:42:34 +0100

    WIP add aperture filter with example

---
+++ sdks/aperture-java/examples/tomcat7/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "aperture-java-tomcat-example"
---
+++ sdks/aperture-java/examples/tomcat7/src/main/java/com/fluxninja/example/servlet/ApertureServlet.java
@@ -0,0 +1,19 @@
+package com.fluxninja.example.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApertureServlet extends HttpServlet {
+
+    public ApertureServlet() {
+        super();
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().append("testString");
+    }
+}
