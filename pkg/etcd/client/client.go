7c1e1a055cedb65bbbc13b0f3924f9a782bd66a0
Tanveer Gill <tanveer.gill@fluxninja.com>
2022-12-19 19:45:29 -0800

    Separate out etcd election from client

--- pkg/etcd/client/client.go
+++ pkg/etcd/client/client.go
@@ -3,7 +3,6 @@ package etcd
 
 import (
 	"context"
-	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
 	concurrencyv3 "go.etcd.io/etcd/client/v3/concurrency"
@@ -13,11 +12,9 @@ import (
 
 	"github.com/fluxninja/aperture/pkg/agentinfo"
 	"github.com/fluxninja/aperture/pkg/config"
-	"github.com/fluxninja/aperture/pkg/info"
 	"github.com/fluxninja/aperture/pkg/log"
 	"github.com/fluxninja/aperture/pkg/net/tlsconfig"
 	"github.com/fluxninja/aperture/pkg/panichandler"
-	"github.com/hashicorp/go-multierror"
 )
 
 // Module is a fx module that provides etcd client.
@@ -67,12 +64,12 @@ type ClientIn struct {
 
 // Client is a wrapper around etcd client v3. It provides interfaces rooted by a namespace in etcd.
 type Client struct {
-	KV       clientv3.KV
-	Watcher  clientv3.Watcher
-	Lease    clientv3.Lease
-	Client   *clientv3.Client
-	Election *concurrencyv3.Election
-	LeaseID  clientv3.LeaseID
+	KV      clientv3.KV
+	Watcher clientv3.Watcher
+	Lease   clientv3.Lease
+	Client  *clientv3.Client
+	Session *concurrencyv3.Session
+	LeaseID clientv3.LeaseID
 }
 
 // ProvideClient creates a new Etcd Client and provides it via Fx.
@@ -120,6 +117,7 @@ func ProvideClient(in ClientIn) (*Client, error) {
 			}
 			etcdClient.Client = cli
 
+			// namespace the client
 			cli.Lease = namespacev3.NewLease(cli.Lease, namespace)
 			etcdClient.Lease = cli.Lease
 			cli.KV = namespacev3.NewKV(cli.KV, namespace)
@@ -134,21 +132,11 @@ func ProvideClient(in ClientIn) (*Client, error) {
 				cancel()
 				return err
 			}
+			etcdClient.Session = session
 			// save the lease id
 			etcdClient.LeaseID = session.Lease()
-			// Create an election for this client
-			etcdClient.Election = concurrencyv3.NewElection(session, "/election/"+in.AgentInfo.GetAgentGroup())
-			// A goroutine to do leader election
+			// A goroutine to check if the session is expired
 			panichandler.Go(func() {
-				// try to elect a leader
-				err := etcdClient.Election.Campaign(ctx, info.GetHostInfo().Hostname)
-				if err != nil {
-					log.Error().Err(err).Msg("Unable to elect a leader")
-					shutdownErr := in.Shutdowner.Shutdown()
-					if shutdownErr != nil {
-						log.Error().Err(shutdownErr).Msg("Error on invoking shutdown")
-					}
-				}
 				// wait for the context to be done or session to be closed
 				select {
 				case <-ctx.Done():
@@ -167,29 +155,9 @@ func ProvideClient(in ClientIn) (*Client, error) {
 		OnStop: func(_ context.Context) error {
 			log.Info().Msg("Closing etcd connections")
 			cancel()
-			var merr error
-			// resign from the election if are the leader
-			if etcdClient.Election.Key() != "" {
-				stopCtx, stopCancel := context.WithTimeout(context.Background(), time.Duration(time.Second*5))
-				err := etcdClient.Election.Resign(stopCtx)
-				stopCancel()
-				if err != nil {
-					log.Error().Err(err).Msg("Unable to resign from the election")
-					merr = multierror.Append(merr, err)
-				}
-			}
-			err := etcdClient.Client.Close()
-			if err != nil {
-				merr = multierror.Append(merr, err)
-			}
-			return merr
+			return etcdClient.Client.Close()
 		},
 	})
 
 	return etcdClient, nil
-}
-
-// IsLeader returns true if the current node is the leader.
-func (c *Client) IsLeader() bool {
-	return c.Election.Key() != ""
 }
