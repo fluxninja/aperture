71b3fc244e2c9279fc0ca8160ab5d27c3631d337
Tanveer Gill <tanveer.gill@fluxninja.com>
2023-01-10 20:53:29 -0800

    copy button

--- docs/content/assets/scripts/version-code.js
+++ docs/content/assets/scripts/version-code.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const VersionCode = (props) => {
+  const codeRef = useRef(null);
+
+  const handleCopyClick = () => {
+    const text = codeRef.current.textContent;
+    navigator.clipboard.writeText(text);
+  };
+
   const childrenAsString = React.Children.toArray(props.children).join("");
   return (
-    <pre>
-      <code>{childrenAsString.replace("${version}", props.version)}</code>
-    </pre>
+    <div>
+      <pre className="highlight" ref={codeRef}>
+        <code>{childrenAsString.replace("${version}", props.version)}</code>
+      </pre>
+      <button className="copy-button" onClick={handleCopyClick}>
+        Copy
+      </button>
+    </div>
   );
 };
 
