52f1dbf5b0b99cae7a6c22cde2641ff7241a83b0
Daria Danilenko <daria.danilenko@fluxninja.com>
2023-01-23 21:34:11 +0100

    Add tests for special values in ConstantSignal

--- pkg/policies/controlplane/runtime/circuit_test.go
+++ pkg/policies/controlplane/runtime/circuit_test.go
@@ -1,6 +1,8 @@
 package runtime_test
 
 import (
+	"math"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -34,6 +36,49 @@ var _ = Describe("Circuit", func() {
 		Expect(circuit.Run(3)).To(Equal(
 			sim.Outputs{
 				"SUM": sim.NewReadings([]float64{1.0, 2.0, 3.0}),
+			},
+		))
+	})
+
+	It("properly generates all kinds of constant signals", func() {
+		circuit, err := sim.NewCircuitFromYaml(
+			`
+			components:
+			- variable:
+				default_config:
+					constant_signal:
+						value: 42
+				out_ports:
+					output: { signal_name: VAR }
+			- variable:
+				default_config:
+					constant_signal:
+						special_value: NaN
+				out_ports:
+					output: { signal_name: VAR2 }
+			- variable:
+				default_config:
+					constant_signal:
+						special_value: +Inf
+				out_ports:
+					output: { signal_name: VAR3 }
+			- variable:
+				default_config:
+					constant_signal:
+						special_value: -Inf
+				out_ports:
+					output: { signal_name: VAR4 }
+			`,
+			sim.Inputs(nil),
+			sim.OutputSignals{"VAR", "VAR2", "VAR3", "VAR4"},
+		)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(circuit.Step()).To(Equal(
+			map[string]sim.Reading{
+				"VAR":  sim.NewReading(42),
+				"VAR2": sim.NewReading(math.NaN()),
+				"VAR3": sim.NewReading(math.Inf(1)),
+				"VAR4": sim.NewReading(math.Inf(-1)),
 			},
 		))
 	})
