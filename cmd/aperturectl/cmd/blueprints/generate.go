433894f49c81d3ca596f30d085a1634c6c7fea8a
Hardik Shingala <34568645+hdkshingala@users.noreply.github.com>
2023-02-03 10:19:45 +0530

    Added support for applying policy on Kubernetes conditionally via aperturectl and use of custom blueprints (#1264)
    ### Description of change
    
    Added below support in `aperturectl`:
    
    - User can pass `--apply` flag which will apply all the valid policies
    generated on Kubernetes
    - User can pass `--custom-blueprints-path` using which they can provide
    any other blueprints than the ones which we pulled.

--- cmd/aperturectl/cmd/blueprints/generate.go
+++ cmd/aperturectl/cmd/blueprints/generate.go
@@ -1,33 +1,53 @@
 package blueprints
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/fluxninja/aperture/cmd/aperturectl/cmd/utils"
+	"github.com/fluxninja/aperture/operator/api"
 	policyv1alpha1 "github.com/fluxninja/aperture/operator/api/policy/v1alpha1"
 	"github.com/fluxninja/aperture/pkg/log"
-	"github.com/goccy/go-graphviz"
 	"github.com/google/go-jsonnet"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/slices"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/yaml"
 )
 
 var (
-	blueprintName string
-	outputDir     string
-	valuesFile    string
-	graphDir      string
+	blueprintName        string
+	outputDir            string
+	valuesFile           string
+	graphDir             string
+	apply                bool
+	kubeConfig           string
+	validPolicies        []*policyv1alpha1.Policy
+	customBlueprintsPath string
 )
 
 func init() {
-	generateCmd.PersistentFlags().StringVar(&blueprintName, "name", "", "Name of the blueprints to generate manifests for")
-	generateCmd.PersistentFlags().StringVar(&outputDir, "output-dir", "", "Directory path where the generated manifests will be stored. If not provided, will use current directory")
-	generateCmd.PersistentFlags().StringVar(&valuesFile, "values-file", "", "Path to the values file for blueprints input")
+	generateCmd.Flags().StringVar(&blueprintName, "name", "", "Name of the blueprints to generate manifests for. Can be skipped when '--custom-blueprints-path' is provided")
+	generateCmd.Flags().StringVar(&outputDir, "output-dir", "", "Directory path where the generated manifests will be stored. If not provided, will use current directory")
+	generateCmd.Flags().StringVar(&valuesFile, "values-file", "", "Path to the values file for blueprints input")
+	generateCmd.Flags().BoolVar(&apply, "apply", false, "Apply policy on the Kubernetes cluster")
+	generateCmd.Flags().StringVar(&kubeConfig, "kube-config", "", "Path to the Kubernets cluster config. Defaults to '~/.kube/config'")
+	generateCmd.Flags().StringVar(&customBlueprintsPath, "custom-blueprints-path", "", "Path to the directory containing custom blueprints")
+
+	validPolicies = []*policyv1alpha1.Policy{}
 }
 
 var generateCmd = &cobra.Command{
@@ -35,7 +55,7 @@ var generateCmd = &cobra.Command{
 	Short:         "Generate manifests for the given blueprint",
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if blueprintName == "" {
+		if blueprintName == "" && customBlueprintsPath == "" {
 			return fmt.Errorf("--name must be provided")
 		}
 
@@ -61,9 +81,33 @@ var generateCmd = &cobra.Command{
 			return err
 		}
 
-		err = blueprintExists(blueprintsVersion, blueprintName)
-		if err != nil {
-			return err
+		if customBlueprintsPath == "" {
+			err = blueprintExists(blueprintsVersion, blueprintName)
+			if err != nil {
+				return err
+			}
+		} else {
+			var fileInfo fs.FileInfo
+			fileInfo, err = os.Stat(customBlueprintsPath)
+			if err != nil {
+				return fmt.Errorf("value provided for --custom-blueprints-path '%s' doesn't exist", customBlueprintsPath)
+			}
+
+			if fileInfo.IsDir() {
+				var files []string
+				files, err = filepath.Glob(filepath.Join(customBlueprintsPath, "*"))
+				if err != nil {
+					return fmt.Errorf("failed to read files in the '%s' directory", customBlueprintsPath)
+				}
+
+				if !slices.Contains(files, filepath.Join(customBlueprintsPath, "config.libsonnet")) || !slices.Contains(files, filepath.Join(customBlueprintsPath, "bundle.libsonnet")) {
+					return fmt.Errorf("value provided for --custom-blueprints-path '%s' is not valid blueprints", customBlueprintsPath)
+				}
+
+				blueprintName = filepath.Base(customBlueprintsPath)
+			} else {
+				return fmt.Errorf("value provided for --custom-blueprints-path '%s' is not a directory", customBlueprintsPath)
+			}
 		}
 
 		apertureBlueprintsDir := filepath.Join(blueprintsDir, blueprintsVersion)
@@ -73,11 +117,18 @@ var generateCmd = &cobra.Command{
 			JPaths: []string{apertureBlueprintsDir},
 		})
 
+		var blueprintsPath string
+		if customBlueprintsPath != "" {
+			blueprintsPath = filepath.Join(customBlueprintsPath, "bundle.libsonnet")
+		} else {
+			blueprintsPath = fmt.Sprintf("github.com/fluxninja/aperture/blueprints/lib/1.0/%s/bundle.libsonnet", blueprintName)
+		}
+
 		jsonStr, err := vm.EvaluateAnonymousSnippet("bundle.libsonnet", fmt.Sprintf(`
-		local bundle = import 'github.com/fluxninja/aperture/blueprints/lib/1.0/%s/bundle.libsonnet';
+		local bundle = import '%s';
 		local config = std.parseYaml(importstr '%s');
 		bundle { _config+:: config }
-		`, blueprintName, valuesFile))
+		`, blueprintsPath, valuesFile))
 		if err != nil {
 			return err
 		}
@@ -102,6 +153,13 @@ var generateCmd = &cobra.Command{
 		}
 
 		log.Info().Msgf("Stored all the manifests at '%s'.", updatedOutputDir)
+
+		if apply {
+			if err = applyPolicy(); err != nil {
+				return err
+			}
+		}
+
 		return nil
 	},
 }
@@ -211,7 +269,7 @@ func generateGraphs(content []byte, contentPath string) error {
 
 	fileName := strings.TrimSuffix(filepath.Base(contentPath), filepath.Ext(contentPath))
 	dotFilePath := filepath.Join(graphDir, fmt.Sprintf("%s.dot", fileName))
-	svgFilePath := filepath.Join(graphDir, fmt.Sprintf("%s.svg", fileName))
+	mmdFilePath := filepath.Join(graphDir, fmt.Sprintf("%s.mmd", fileName))
 
 	policyFile, err := utils.FetchPolicyFromCR(contentPath)
 	if err != nil {
@@ -228,19 +286,86 @@ func generateGraphs(content []byte, contentPath string) error {
 		return err
 	}
 
-	graphBytes, err := os.ReadFile(dotFilePath)
+	if err = utils.GenerateMermaidFile(circuit, mmdFilePath); err != nil {
+		return err
+	}
+
+	validPolicies = append(validPolicies, policy)
+	return nil
+}
+
+func applyPolicy() error {
+	if kubeConfig == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		kubeConfig = filepath.Join(homeDir, ".kube", "config")
+		fmt.Printf("Using '%s' as Kubernetes config\n", kubeConfig)
+	}
+
+	kubeRestConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	if err != nil {
+		return fmt.Errorf("failed to connect to Kubernetes. Error: %s", err.Error())
+	}
+
+	return createPolicy(kubeRestConfig)
+}
+
+func getControllerDeployment(kubeRestConfig *rest.Config) (*appsv1.Deployment, error) {
+	clientSet, err := kubernetes.NewForConfig(kubeRestConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to Kubernetes. Error: %s", err.Error())
+	}
+
+	deployment, err := clientSet.AppsV1().Deployments("").List(context.Background(), metav1.ListOptions{
+		FieldSelector: "metadata.name=aperture-controller",
+	})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil, fmt.Errorf(
+				"no deployment with name 'aperture-controller' found on the Kubernetes cluster. The policy can be only applied in the namespace where the Aperture Controller is running")
+		}
+		return nil, fmt.Errorf("failed to fetch aperture-controller namespace in Kubernetes. Error: %s", err.Error())
+	}
+
+	if len(deployment.Items) != 1 {
+		return nil, fmt.Errorf(
+			"no deployment with name 'aperture-controller' found on the Kubernetes cluster. The policy can be only applied in the namespace where the Aperture Controller is running")
+	}
+
+	return &deployment.Items[0], nil
+}
+
+func createPolicy(kubeRestConfig *rest.Config) error {
+	deployment, err := getControllerDeployment(kubeRestConfig)
 	if err != nil {
 		return err
 	}
 
-	g := graphviz.New()
-	graph, err := graphviz.ParseBytes(graphBytes)
+	err = api.AddToScheme(scheme.Scheme)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to connect to Kubernetes. Error: %s", err.Error())
+	}
+	client, err := client.New(kubeRestConfig, client.Options{
+		Scheme: scheme.Scheme,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to connect to Kubernetes. Error: %s", err.Error())
 	}
 
-	if err := g.RenderFilename(graph, graphviz.SVG, svgFilePath); err != nil {
-		return err
+	for _, policy := range validPolicies {
+		policy.Namespace = deployment.GetNamespace()
+		spec := policy.Spec
+		_, err = controllerutil.CreateOrUpdate(context.Background(), client, policy, func() error {
+			policy.Spec = spec
+			return nil
+		})
+		if err != nil {
+			return fmt.Errorf("failed to apply policy in Kubernetes. Error: %s", err.Error())
+		}
+
+		log.Info().Msgf("Applied policy '%s' in '%s' namespace.\n", policy.GetName(), policy.GetNamespace())
 	}
 	return nil
 }
