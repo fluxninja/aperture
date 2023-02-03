75a5c74fa504d8a09e810be3b2087ffd036d6928
Harjot Gill <harjot.gill@fluxninja.com>
2023-02-03 15:19:38 -0800

    docs: move doc-gen to docs/tools

--- scripts/precommit/generate_cli_docs.sh
+++ scripts/precommit/generate_cli_docs.sh
@@ -5,6 +5,6 @@ gitroot="$(git rev-parse --show-toplevel)"
 
 pushd "$gitroot"/docs >/dev/null
 rm -rf ./content/reference/aperture-cli/aperturectl*.md
-go run ../cmd/aperturectl/gen-docs/generate-docs.go
+go run ./tools/aperturectl/generate-docs.go
 npx prettier --prose-wrap="preserve" ./content/reference/aperture-cli/ --write
 popd >/dev/null
