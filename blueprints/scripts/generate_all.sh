fb6416edc04ddd5ee02d56e9c3cb6ab68aef178f
Harjot Gill <harjot.gill@fluxninja.com>
2023-02-03 21:50:25 -0800

    remove examples and old blueprint generator

--- blueprints/scripts/generate_all.sh
+++ blueprints/scripts/generate_all.sh
@@ -12,14 +12,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	SED="gsed"
 fi
 
-# accept a --force flag to force the regeneration of all graphs
-# check if $1 is bounded and equal to --force
-if [[ -n "${1:-}" ]] && [[ "$1" == "--force" ]]; then
-	force=true
-else
-	force=false
-fi
-
 # run jb install in the blueprints_root
 pushd "${blueprints_root}" >/dev/null
 jb install
@@ -79,35 +71,4 @@ $FIND "$blueprints_root"/lib/1.0 -type f -name config.libsonnet | while read -r 
 	tail -n +2 "$dir"/README.md >>"$docs_file"
 	# run prettier on the docs file
 	npx prettier --write "$docs_file"
-done
-
-# for all directories within "$blueprints_root"/bundles, generate examples
-$FIND "$blueprints_root"/examples -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
-	echo "Generating examples for $dir"
-	# save the contents of $dir/gen/policies/example.yaml for comparison
-	old_example_yaml=$(cat "$dir"/gen/policies/example.yaml 2>/dev/null || true)
-
-	# generate example blueprint
-	python "${blueprints_root}"/scripts/generate-bundle.py --output "$dir"/gen/ \
-		--config "$dir"/example.jsonnet
-
-	npx prettier --write "$dir"/gen/.. || true
-
-	new_example_yaml=$(cat "$dir"/gen/policies/example.yaml)
-
-	if [[ "$old_example_yaml" != "$new_example_yaml" || "$force" == true ]]; then
-		mkdir -p "$dir"/gen/graph
-		# fail if commands below fails
-		go run -mod=mod "${blueprints_root}"/../cmd/aperturectl/main.go compile \
-			--cr "$dir"/gen/policies/example.yaml \
-			--dot "$dir"/gen/graph/graph.dot
-		# if exit code is not 0 then remove example.yaml
-		# shellcheck disable=SC2181
-		if [[ $? -ne 0 ]]; then
-			rm "$dir"/gen/policies/example.yaml
-			# exit script with error
-			exit 1
-		fi
-		dot -Tsvg "$dir"/gen/graph/graph.dot >"$dir"/gen/graph/graph.svg
-	fi
 done
