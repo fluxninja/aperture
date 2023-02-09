83631db5ff3a897628183dddbb4c949a45c0f5ef
Filip Chmielewski <IridiumOxide@users.noreply.github.com>
2023-02-09 12:13:23 +0100

    Javaagent advanced configuration (#1321)

--- sdks/aperture-java/javaagent/agent/src/main/java/com/fluxninja/aperture/instrumentation/netty/NettyServerAdvice.java
+++ sdks/aperture-java/javaagent/agent/src/main/java/com/fluxninja/aperture/instrumentation/netty/NettyServerAdvice.java
@@ -1,5 +1,6 @@
 package com.fluxninja.aperture.instrumentation.netty;
 
+import com.fluxninja.aperture.instrumentation.Config;
 import com.fluxninja.aperture.netty.ApertureServerHandler;
 import com.fluxninja.aperture.sdk.ApertureSDK;
 import io.netty.channel.ChannelHandler;
@@ -9,10 +10,8 @@ import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpServerCodec;
 import net.bytebuddy.asm.Advice;
 
-import java.time.Duration;
-
 public class NettyServerAdvice {
-    public static ApertureSDK apertureSDK = apertureSDKFromConfig();
+    public static ApertureSDK apertureSDK = Config.newSDKFromConfig();
 
     @Advice.OnMethodExit
     public static void onExit(
@@ -30,28 +29,5 @@ public class NettyServerAdvice {
             }
             pipeline.addAfter(hname, apertureHandler.getClass().getName(), apertureHandler);
         }
-    }
-
-    private static ApertureSDK apertureSDKFromConfig() {
-        String host = System.getProperty("aperture.agent.hostname");
-        String port = System.getProperty("aperture.agent.port");
-        if (host == null) {
-            host = "localhost";
-        }
-        if (port == null) {
-            port = "8089";
-        }
-        ApertureSDK sdk;
-        try {
-            sdk = ApertureSDK.builder()
-                    .setHost(host)
-                    .setPort(Integer.parseInt(port))
-                    .setDuration(Duration.ofMillis(1000))
-                    .build();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("fail");
-        }
-        return sdk;
     }
 }
