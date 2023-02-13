60f739a9cf29114249f2e08e36b9c4630526b469
Michał Krasnoborski <michal.krasnoborski@fluxninja.com>
2023-02-13 15:01:08 +0100

    Add proto definitions and helpers for controlpoint convertions

--- pkg/policies/flowcontrol/selectors/control-point-id.go
+++ pkg/policies/flowcontrol/selectors/control-point-id.go
@@ -1,10 +1,14 @@
 package selectors
 
 import (
+	cmdv1 "github.com/fluxninja/aperture/api/gen/proto/go/aperture/cmd/v1"
 	policylangv1 "github.com/fluxninja/aperture/api/gen/proto/go/aperture/policy/language/v1"
 )
 
 // ControlPointID is the struct that represents a ControlPoint.
+//
+// Note: We need to mirror cmdv1.ServiceControlPoint, because
+// protobuf-generated struct cannot be used as map keys.
 type ControlPointID struct {
 	Service      string
 	ControlPoint string
@@ -15,6 +19,22 @@ func NewControlPointID(service string, controlPoint string) ControlPointID {
 	return ControlPointID{
 		Service:      service,
 		ControlPoint: controlPoint,
+	}
+}
+
+// ToProto returns protobuf representation of control point
+func (cp *ControlPointID) ToProto() *cmdv1.ServiceControlPoint {
+	return &cmdv1.ServiceControlPoint{
+		ServiceName: cp.Service,
+		Name:        cp.ControlPoint,
+	}
+}
+
+// ControlPointIDFromProto creates ControlPointID from protobuf representation
+func ControlPointIDFromProto(protoCP *cmdv1.ServiceControlPoint) ControlPointID {
+	return ControlPointID{
+		Service:      protoCP.GetServiceName(),
+		ControlPoint: protoCP.GetName(),
 	}
 }
 
