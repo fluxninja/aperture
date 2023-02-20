911c97a1535cd5847ef307a707df69c62d00c26e
Krzysztof Klimonda <krzysztof.klimonda@fluxninja.com>
2023-02-20 16:14:13 +0100

    Update graphql-example-1 scenario load generator config

--- playground/scenarios/graphql-example-1/load_generator/test.js
+++ playground/scenarios/graphql-example-1/load_generator/test.js
@@ -2,6 +2,8 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 import { vu } from "k6/execution";
+import crypto from "k6/crypto";
+import encoding from "k6/encoding";
 
 export let options = {
     discardResponseBodies: true,
@@ -17,15 +19,35 @@ export let options = {
     },
 };
 
+function sign(data, hashAlg, secret) {
+    let hasher = crypto.createHMAC(hashAlg, secret);
+    hasher.update(data);
+
+    // Some manual base64 rawurl encoding as `Hasher.digest(encodingType)`
+    // doesn't support that encoding type yet.
+    return hasher.digest("base64").replace(/\//g, "_").replace(/\+/g, "-").replace(/=/g, "");
+}
+
+function encode(payload, secret) {
+    let header = encoding.b64encode(JSON.stringify({ typ: "JWT", alg: "HS256" }), "rawurl");
+    payload = encoding.b64encode(JSON.stringify(payload), "rawurl");
+    let sig = sign(header + "." + payload, "sha256", secret);
+    return [header, payload, sig].join(".");
+}
+
 export default function () {
-    let userId = vu.idInTest;
+    let message = {
+        userID: "" + vu.idInTest,
+    }
+    let jwt = encode(message, "secret");
     const url = "http://service-graphql-demo-app.demoapp.svc.cluster.local/query";
     const headers = {
         "Content-Type": "application/json",
+        "Authorization": "Bearer " + jwt,
         Cookie: "session=eyJ1c2VyIjoia2Vub2JpIn0.YbsY4Q.kTaKRTyOIfVlIbNB48d9YH6Q0wo",
     };
     const mutation = `mutation createTodo {
-        createTodo(input: { text: "todo", userId: "${userId}" }) {
+        createTodo(input: { text: "todo" }) {
           user {
             id
           }
