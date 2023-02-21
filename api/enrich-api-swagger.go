8441f586f35f21ac1a123d06e78352d12a294a85
Harjot Gill <harjot.gill@fluxninja.com>
2023-02-21 14:59:44 -0800

    gen: enrich api swagger with gotags extensions automatically

--- api/enrich-api-swagger.go
+++ api/enrich-api-swagger.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strconv"
 	"strings"
 
 	"gopkg.in/yaml.v3"
@@ -59,6 +60,9 @@ func main() {
 	// search for $ref key in entire swagger and replace with new name
 	replaceRef(swagger, replaceRefs)
 
+	// process gotags
+	processGoTags(swagger)
+
 	// encode as YAML
 	swaggerBytes, err = yaml.Marshal(swagger)
 	if err != nil {
@@ -95,4 +99,99 @@ func replaceRef(content map[string]interface{}, replacements map[string]string) 
 			}
 		}
 	}
+}
+
+func processGoTags(content map[string]interface{}) {
+	// look for "@gotags: " line in the description
+	// example of gotags annotation: "@gotags: default:"info" validate:"oneof=info warn crit"
+	for k, v := range content {
+		if k == "description" {
+			desc, ok := v.(string)
+			if !ok {
+				continue
+			}
+			// split desc into separate lines
+			lines := strings.Split(desc, "\n")
+			// look for "@gotags: " line
+			for _, line := range lines {
+				prefix := "@gotags: "
+				// look for "@gotags:" line
+				if !strings.HasPrefix(line, prefix) {
+					continue
+				}
+				// remove "@gotags: " prefix
+				tags := line[len(prefix):]
+				tagMap := parseStructTag(tags)
+				// add each tag as "x-go-tag-<tagname>" key
+				for k1, v1 := range tagMap {
+					content["x-go-tag-"+k1] = v1
+				}
+			}
+		}
+		// dive into the map
+		if m, ok := v.(map[string]interface{}); ok {
+			processGoTags(m)
+		}
+		// dive into the array
+		if a, ok := v.([]interface{}); ok {
+			for _, v1 := range a {
+				if m, ok := v1.(map[string]interface{}); ok {
+					processGoTags(m)
+				}
+			}
+		}
+	}
+}
+
+func parseStructTag(tags string) map[string]string {
+	tagMap := make(map[string]string)
+
+	for tags != "" {
+		// Skip leading space.
+		i := 0
+		for i < len(tags) && tags[i] == ' ' {
+			i++
+		}
+		tags = tags[i:]
+		if tags == "" {
+			break
+		}
+
+		// Scan to colon. A space, a quote or a control character is a syntax error.
+		// Strictly speaking, control chars include the range [0x7f, 0x9f], not just
+		// [0x00, 0x1f], but in practice, we ignore the multi-byte control characters
+		// as it is simpler to inspect the tag's bytes than the tag's runes.
+		i = 0
+		for i < len(tags) && tags[i] > ' ' && tags[i] != ':' && tags[i] != '"' && tags[i] != 0x7f {
+			i++
+		}
+		if i == 0 || i+1 >= len(tags) || tags[i] != ':' || tags[i+1] != '"' {
+			break
+		}
+		name := string(tags[:i])
+		tags = tags[i+1:]
+
+		// Scan quoted string to find value.
+		i = 1
+		for i < len(tags) && tags[i] != '"' {
+			if tags[i] == '\\' {
+				i++
+			}
+			i++
+		}
+		if i >= len(tags) {
+			break
+		}
+		qvalue := string(tags[:i+1])
+		tags = tags[i+1:]
+
+		value, err := strconv.Unquote(qvalue)
+		if err != nil {
+			continue
+		}
+
+		tagMap[name] = value
+	}
+
+	return tagMap
 }
