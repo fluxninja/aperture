05c041917b3a6de8c3fb2966fa6eee4a1885cd88
Harjot Gill <harjot.gill@fluxninja.com>
2023-02-22 23:16:22 -0800

    use parallel command to speed up jsonnet generation and validation

--- docs/tools/mermaid/generate_mermaid.sh
+++ docs/tools/mermaid/generate_mermaid.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 gitroot=$(git rev-parse --show-toplevel)
+export gitroot
+
 docsdir=$gitroot/docs
+export docsdir
 
 GREP="grep"
 SED="sed"
 AWK="awk"
-
+FIND="find"
 # check whether we are using macOS
 if [ "$(uname)" == "Darwin" ]; then
 	GREP="ggrep"
 	SED="gsed"
 	AWK="gawk"
+	FIND="gfind"
 fi
+export GREP
+export SED
+export AWK
+export FIND
 
 # accept a --force flag to force generation of all mermaid files
 if [[ -n "${1:-}" ]] && [[ "$1" == "--force" ]]; then
@@ -20,27 +28,28 @@ if [[ -n "${1:-}" ]] && [[ "$1" == "--force" ]]; then
 else
 	force=false
 fi
+export force
 
 # script that parses all markdown (md) files recursively in a directory
 # and extracts content for the mermaid sections that begin with ```mermaid and end with ```
 # and generates mmd files for each mermaid section
-rm -rf tmp
-mkdir -p tmp
-files=$(find "$docsdir"/content -type f -name "*.md")
-for f in $files; do
+function generate_mermaid() {
+	set -euo pipefail
+	f="$1"
 	dir=$(dirname "$f")
 	filename=$(basename "$f")
 	filenameNoExt="${filename%.*}"
 	out_dir="$dir"/assets/gen/"$filenameNoExt"
 	rm -f "$out_dir"/*.mmd
+	tmpdir=$(mktemp -d)
 	# extract all mermaid multiline sections that start with ```mermaid and end with ``` into an array of sections
 	#shellcheck disable=SC2002,SC2016
-	cat "$f" | $SED -n '/```mermaid/,/```/p' | $GREP -vP '^```$' >tmp/records.txt
+	cat "$f" | $SED -n '/```mermaid/,/```/p' | $GREP -vP '^```$' >"$tmpdir"/records.txt
 	# use awk to separate out mermaid_records using RS='```' into an array of sections
 	#shellcheck disable=SC2016
-	$AWK '{RS="```mermaid"} NR > 1 { print $0 > "tmp/mermaid_section_" ++i}' tmp/records.txt
+	$AWK -v tmpdir="$tmpdir" '{RS="```mermaid"} NR > 1 { print $0 > tmpdir"/mermaid_section_" ++i}' "$tmpdir"/records.txt
 	# find mermaid_section_* and sort them
-	mermaid_section_files=$(find tmp -type f -name "mermaid_section_*" | sort -n)
+	mermaid_section_files=$($FIND "$tmpdir" -type f -name "mermaid_section_*" | sort -n)
 	count=0
 	for mermaid_section_file in $mermaid_section_files; do
 		# skip this file if it contains "@include:"
@@ -64,20 +73,20 @@ for f in $files; do
 		# generate mmd
 		echo "generating $outfilename"
 		mv "$mermaid_section_file" "$out_dir"/"$outfilename"
-		git add "$out_dir"/"$outfilename"
 		# increment count
 		count=$((count + 1))
 	done
-	rm -rf tmp/*
-done
+	rm -rf "$tmpdir"
+}
 
-rm -rf tmp
+export -f generate_mermaid
+
+parallel -j4 --no-notice --bar --eta generate_mermaid ::: "$($FIND "$docsdir"/content -type f -name "*.md")"
 
 # find all mmd files and generate svg and png files only when mmd contents change (using md5sum)
-
-# find all mmd files
-mmd_files=$(find "$docsdir"/content -type f -name "*.mmd")
-for mmd_file in $mmd_files; do
+function generate_mermaid_images() {
+	set -euo pipefail
+	mmd_file="$1"
 	# generate svg and png files only when mmd contents change (using md5sum)
 	#shellcheck disable=SC2016
 	md5sum=$(md5sum "$mmd_file" | $AWK '{print $1}')
@@ -91,10 +100,12 @@ for mmd_file in $mmd_files; do
 		for fmt in svg; do #png; do
 			npx -p @mermaid-js/mermaid-cli mmdc \
 				--quiet --input "$mmd_file" --configFile "$docsdir"/tools/mermaid/mermaid-theme.json --cssFile "$docsdir"/tools/mermaid/mermaid.css --scale 2 --output "$mmd_file"."$fmt" --backgroundColor transparent
-			git add "$mmd_file"."$fmt"
 		done
 		# update md5sum
 		echo "$md5sum" >"$mmd_file".md5sum
-		git add "$mmd_file".md5sum
 	fi
-done
+}
+
+export -f generate_mermaid_images
+
+parallel -j4 --no-notice --bar --eta generate_mermaid_images ::: "$($FIND "$docsdir"/content -type f -name "*.mmd")"
