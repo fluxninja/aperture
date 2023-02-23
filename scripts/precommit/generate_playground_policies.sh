ad86194b3785f0f5f8006dba1d38b5b7f0a494ab
Harjot Gill <harjot.gill@fluxninja.com>
2023-02-22 23:20:46 -0800

    gen playground in parallel

--- scripts/precommit/generate_playground_policies.sh
+++ scripts/precommit/generate_playground_policies.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+FIND="find"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	FIND="gfind"
+fi
+export FIND
+
 gitroot="$(git rev-parse --show-toplevel)"
+export gitroot
+
 aperturectl="$("$gitroot"/playground/scripts/build_aperturectl.sh)"
+export aperturectl
 
 pushd "$gitroot" >/dev/null
-find playground -type f -name metadata.json | while IFS= read -r scenario; do
-    scenario_dir=$(dirname "$scenario")
-    rm -rf "$scenario_dir"/**/*-cr.yaml
-    metadata_file="$scenario_dir"/metadata.json
-    readarray -t policies < <(jq --compact-output '.aperture_policies[]' "$metadata_file")
-    for policy in "${policies[@]}"; do
-        policy_name=$(jq --raw-output '.policy_name' <<< "$policy")
-        policy_def=$(jq --raw-output '.policy_def' <<< "$policy")
-        values_file=$(jq --raw-output '.values_file' <<< "$policy")
-        echo "Generating policies: $policy_name"
-        "$gitroot"/playground/scripts/render-policy.sh "$gitroot/playground" "$aperturectl" "$gitroot/blueprints" "$policy_def" "$policy_name" "$scenario_dir"/"$values_file" >/dev/null
-    done
-done
+
+function generate_policies() {
+	set -euo pipefail
+	scenario="$1"
+	scenario_dir=$(dirname "$scenario")
+	rm -rf "$scenario_dir"/**/*-cr.yaml
+	metadata_file="$scenario_dir"/metadata.json
+	readarray -t policies < <(jq --compact-output '.aperture_policies[]' "$metadata_file")
+	for policy in "${policies[@]}"; do
+		policy_name=$(jq --raw-output '.policy_name' <<<"$policy")
+		policy_def=$(jq --raw-output '.policy_def' <<<"$policy")
+		values_file=$(jq --raw-output '.values_file' <<<"$policy")
+		echo "Generating policies: $policy_name"
+		"$gitroot"/playground/scripts/render-policy.sh "$gitroot/playground" "$aperturectl" "$gitroot/blueprints" "$policy_def" "$policy_name" "$scenario_dir"/"$values_file" >/dev/null
+	done
+}
+
+export -f generate_policies
+
+parallel -j4 --no-notice --bar --eta generate_policies ::: "$($FIND playground -type f -name metadata.json)"
+
 popd >/dev/null
