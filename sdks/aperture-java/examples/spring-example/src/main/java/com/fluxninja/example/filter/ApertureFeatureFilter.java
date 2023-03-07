b856d385e655b061df764bf6e70ffc000a1e20c1
Filip Chmielewski <IridiumOxide@users.noreply.github.com>
2023-03-07 15:49:32 +0100

    Downgrade spring example to work with Java 8 (#1535)

--- sdks/aperture-java/examples/spring-example/src/main/java/com/fluxninja/example/filter/ApertureFeatureFilter.java
+++ sdks/aperture-java/examples/spring-example/src/main/java/com/fluxninja/example/filter/ApertureFeatureFilter.java
@@ -5,14 +5,14 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.fluxninja.aperture.sdk.ApertureSDK;
 import com.fluxninja.aperture.sdk.ApertureSDKException;
