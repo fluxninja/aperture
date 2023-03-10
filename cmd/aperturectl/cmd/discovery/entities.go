13091f3011a8175375879aa27fb46701339329a3
Hasit Mistry <hasit@fluxninja.com>
2023-03-09 18:18:19 -0800

    Add ability to list discovery entities in aperturectl
    - Add `discovery entities` command that list all entities by IP addresses
    - Add `--find-by="[name|ip]=<string>" flag to get by name or ip

---
+++ cmd/aperturectl/cmd/discovery/entities.go
@@ -0,0 +1,82 @@
+package discovery
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	cmdv1 "github.com/fluxninja/aperture/api/gen/proto/go/aperture/cmd/v1"
+)
+
+var findBy string
+
+func init() {
+	EntitiesCmd.Flags().StringVar(&findBy, "find-by", "", "Find entity by [name|ip]")
+}
+
+// EntitiesCmd is the command to list control points.
+var EntitiesCmd = &cobra.Command{
+	Use:           "entities",
+	Short:         "List AutoScale control points",
+	Long:          `List AutoScale control points`,
+	SilenceErrors: true,
+	RunE: func(_ *cobra.Command, _ []string) error {
+		client, err := controller.Client()
+		if err != nil {
+			return err
+		}
+
+		toPrint := ""
+
+		if findBy != "" {
+			find := strings.Split(findBy, "=")
+			if len(find) != 2 {
+				return fmt.Errorf("invalid findBy argument: %s", findBy)
+			}
+
+			switch find[0] {
+			case "name":
+				resp, err := client.ListDiscoveryEntity(context.Background(), &cmdv1.ListDiscoveryEntityRequest{By: &cmdv1.ListDiscoveryEntityRequest_Name{Name: find[1]}})
+				if err != nil {
+					return err
+				}
+				entity, err := protojson.MarshalOptions{Multiline: true, Indent: "  "}.Marshal(resp.Entity)
+				if err != nil {
+					return err
+				}
+				toPrint = string(entity)
+			case "ip":
+				resp, err := client.ListDiscoveryEntity(context.Background(), &cmdv1.ListDiscoveryEntityRequest{By: &cmdv1.ListDiscoveryEntityRequest_IpAddress{IpAddress: find[1]}})
+				if err != nil {
+					return err
+				}
+				entity, err := protojson.MarshalOptions{Multiline: true, Indent: "  "}.Marshal(resp.Entity)
+				if err != nil {
+					return err
+				}
+				toPrint = string(entity)
+			}
+		} else {
+			resp, err := client.ListDiscoveryEntities(context.Background(), &cmdv1.ListDiscoveryEntitiesRequest{})
+			if err != nil {
+				return err
+			}
+			if resp.ErrorsCount != 0 {
+				fmt.Fprintf(os.Stderr, "Could not get answer from %d agents", resp.ErrorsCount)
+			}
+			entities, err := protojson.MarshalOptions{Multiline: true, Indent: "  "}.Marshal(resp.Entities)
+			if err != nil {
+				return err
+			}
+			toPrint = string(entities)
+		}
+
+		fmt.Printf("%s\n", toPrint)
+
+		return nil
+	},
+}
---
+++ docs/content/reference/aperturectl/discovery/entities/entities.md
@@ -0,0 +1,39 @@
+---
+sidebar_label: Entities
+hide_title: true
+keywords:
+  - aperturectl
+  - aperturectl_discovery_entities
+---
+
+## aperturectl discovery entities
+
+List AutoScale control points
+
+### Synopsis
+
+List AutoScale control points
+
+```
+aperturectl discovery entities [flags]
+```
+
+### Options
+
+```
+      --find-by string   Find entity by [name|ip]
+  -h, --help             help for entities
+```
+
+### Options inherited from parent commands
+
+```
+      --controller string    Address of Aperture controller
+      --insecure             Allow insecure connection to controller
+      --kube                 Find controller in Kubernetes cluster, instead of connecting directly
+      --kube-config string   Path to the Kubernetes cluster config. Defaults to '~/.kube/config' or $KUBECONFIG
+```
+
+### SEE ALSO
+
+- [aperturectl discovery](/reference/aperturectl/discovery/discovery.md) - Discovery integrations
