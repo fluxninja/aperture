17eaea044b4b4000154ae96025f7c245c97b42de
Szymon Bieńkowski <szymon.bienkowski@fluxninja.com>
2023-03-20 11:19:41 +0100

    Add java to asdf tools

--- scripts/precommit/java_spotless_lint.sh
+++ scripts/precommit/java_spotless_lint.sh
@@ -4,9 +4,13 @@ set -uo pipefail
 javasdk=$(git rev-parse --show-toplevel)/sdks/aperture-java
 cd "$javasdk" || exit 1
 
-./gradlew spotlessCheck
-code=$?
-
-./gradlew spotlessApply
+code=0
+if ./gradlew spotlessCheck; then
+  echo "Java code is formatted correctly"
+else
+  code=$?
+  echo "Formatting Java code"
+  ./gradlew spotlessApply
+fi
 
 exit $code
