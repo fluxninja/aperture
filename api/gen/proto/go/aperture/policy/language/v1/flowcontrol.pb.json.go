4b81e76b35899044123c17216eca50494b938306
Harjot Gill <harjot.gill@fluxninja.com>
2023-03-27 22:29:04 -0700

    add flowcontrolresources and add deprecation tooling (#1632)
    - New Feature: Adds a new message `FlowControlResources` for flow
    control resources and deprecates old fields in favor of a new one.
    - Chore: Adds deprecation tooling for backwards compatibility.

--- api/gen/proto/go/aperture/policy/language/v1/flowcontrol.pb.json.go
+++ api/gen/proto/go/aperture/policy/language/v1/flowcontrol.pb.json.go
@@ -56,6 +56,22 @@ func (msg *FlowMatcher) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *FlowControlResources) MarshalJSON() ([]byte, error) {
+	return protojson.MarshalOptions{
+		UseEnumNumbers:  false,
+		EmitUnpopulated: false,
+		UseProtoNames:   true,
+	}.Marshal(msg)
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *FlowControlResources) UnmarshalJSON(b []byte) error {
+	return protojson.UnmarshalOptions{
+		DiscardUnknown: false,
+	}.Unmarshal(b, msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *FluxMeter) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{
 		UseEnumNumbers:  false,
--- api/gen/proto/go/aperture/policy/language/v1/policy.pb.validate.go
+++ api/gen/proto/go/aperture/policy/language/v1/policy.pb.validate.go
@@ -730,6 +730,35 @@ func (m *Resources) validate(all bool) error {
 
 	}
 
+	if all {
+		switch v := interface{}(m.GetFlowControl()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ResourcesValidationError{
+					field:  "FlowControl",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ResourcesValidationError{
+					field:  "FlowControl",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetFlowControl()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ResourcesValidationError{
+				field:  "FlowControl",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return ResourcesMultiError(errors)
 	}
