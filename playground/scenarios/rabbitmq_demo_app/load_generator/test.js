93eb289b596a9f632c8d97595aadcd197d5bb4db
Hasit Mistry <hasit@fluxninja.com>
2023-03-30 16:15:48 -0700

    Update rabbitmq_demo_app load scenario

--- playground/scenarios/rabbitmq_demo_app/load_generator/test.js
+++ playground/scenarios/rabbitmq_demo_app/load_generator/test.js
@@ -1,36 +1,17 @@
-
 import http from "k6/http";
 import { check, sleep } from "k6";
 import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 import { vu } from "k6/execution";
 import encoding from "k6/encoding";
 
-export let vuStages = [
-  { duration: "10s", target: 5 },
-  { duration: "2m", target: 5 },
-  { duration: "1m", target: 30 },
-  { duration: "2m", target: 30 },
-  { duration: "10s", target: 5 },
-  { duration: "2m", target: 5 },
-];
-
 export let options = {
   discardResponseBodies: true,
   scenarios: {
-    guests: {
-      executor: "ramping-vus",
-      stages: vuStages,
-      env: { USER_TYPE: "guest" },
-    },
-    subscribers: {
-      executor: "ramping-vus",
-      stages: vuStages,
-      env: { USER_TYPE: "subscriber" },
-    },
-    premium: {
-      executor: "ramping-vus",
-      stages: vuStages,
-      env: { USER_TYPE: "premium" },
+    user: {
+      executor: "constant-vus",
+      vus: 1,
+      duration: "30s",
+      env: { USER_TYPE: "user" },
     },
   },
 };
