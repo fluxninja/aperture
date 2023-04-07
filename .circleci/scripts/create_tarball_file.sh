7515c645cc48f2d0057991697bf6c60641f8c656
slayer321 <sachin.maurya@fluxninja.com>
2023-04-07 14:36:20 +0530

    chatgpt suggested changes

--- .circleci/scripts/create_tarball_file.sh
+++ .circleci/scripts/create_tarball_file.sh
@@ -4,11 +4,11 @@ set -e
 mkdir buildtardir
 cp LICENSE ./buildtardir
 cp ./dist/aperturectl ./buildtardir
-cd  ./buildtardir
-rm_v=${APERTURECTL_BUILD_VERSION}
-rm_v=${rm_v#v}
-touch aperturectl-"${rm_v}"-"${GOOS}"-"${GOARCH}".tar.gz
-tar --exclude=aperturectl-"${rm_v}"-"${GOOS}"-"${GOARCH}".tar.gz -czvf aperturectl-"${rm_v}"-"${GOOS}"-"${GOARCH}".tar.gz .
+cd "./buildtardir"
+version_without_v_prefix=${APERTURECTL_BUILD_VERSION}
+version_without_v_prefix=${version_without_v_prefix#v}
+touch aperturectl-"${version_without_v_prefix}"-"${GOOS}"-"${GOARCH}".tar.gz
+tar --exclude=aperturectl-"${version_without_v_prefix}"-"${GOOS}"-"${GOARCH}".tar.gz -czvf aperturectl-"${version_without_v_prefix}"-"${GOOS}"-"${GOARCH}".tar.gz .
 cp -r -- *.tar.gz "$HOME"/project/dist/packages/
 #Remove the .deb and .rpm package for darwin OS.
 #As this packges don't work on macos and we have .tar.gz file which has aperturectl compiled binary for this systems.
