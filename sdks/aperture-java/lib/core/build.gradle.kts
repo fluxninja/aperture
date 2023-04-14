d133f62123e1dab0df974589e44c95bb1001c108
Hasit Mistry <hasit@fluxninja.com>
2023-04-14 11:28:06 -0700

    Revert io.grpc versions

--- sdks/aperture-java/lib/core/build.gradle.kts
+++ sdks/aperture-java/lib/core/build.gradle.kts
@@ -24,16 +24,16 @@ dependencies {
     implementation("io.opentelemetry:opentelemetry-sdk-trace:1.18.0")
     implementation("io.opentelemetry:opentelemetry-exporter-otlp:1.18.0")
     implementation("io.opentelemetry:opentelemetry-exporter-logging:1.18.0")
-    implementation("io.grpc:grpc-protobuf:1.54.0")
-    implementation("io.grpc:grpc-stub:1.54.0")
+    implementation("io.grpc:grpc-protobuf:1.44.0")
+    implementation("io.grpc:grpc-stub:1.44.0")
     implementation("org.slf4j:slf4j-simple:1.7.0")
 
-    runtimeOnly("io.grpc:grpc-netty-shaded:1.54.0")
+    runtimeOnly("io.grpc:grpc-netty-shaded:1.49.0")
 
     // Workaround for @javax.annotation.Generated
     // see: https://github.com/grpc/grpc-java/issues/3633
     compileOnly("javax.annotation:javax.annotation-api:1.3.2")
-    compileOnly("io.grpc:grpc-api:1.54.0")
+    compileOnly("io.grpc:grpc-api:1.44.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
