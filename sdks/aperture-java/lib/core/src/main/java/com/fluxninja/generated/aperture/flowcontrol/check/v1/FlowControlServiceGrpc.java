d6dbb8047583beca08a1488b8506c976640ca614
Harjot Gill <harjot.gill@fluxninja.com>
2023-04-17 16:07:04 -0700

    pre_commit: regenerate proto (#1756)

--- sdks/aperture-java/lib/core/src/main/java/com/fluxninja/generated/aperture/flowcontrol/check/v1/FlowControlServiceGrpc.java
+++ sdks/aperture-java/lib/core/src/main/java/com/fluxninja/generated/aperture/flowcontrol/check/v1/FlowControlServiceGrpc.java
@@ -8,7 +8,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.54.0)",
+    value = "by gRPC proto compiler (version 1.54.1)",
     comments = "Source: aperture/flowcontrol/check/v1/check.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class FlowControlServiceGrpc {
