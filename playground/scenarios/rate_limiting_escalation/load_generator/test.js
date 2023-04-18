209035462e0fb9ece14ec2faebacad9c2f93114a
Harjot Gill <harjot.gill@fluxninja.com>
2023-04-18 08:43:57 -0700

    playground: add back imports (#1760)

--- playground/scenarios/rate_limiting_escalation/load_generator/test.js
+++ playground/scenarios/rate_limiting_escalation/load_generator/test.js
@@ -1,4 +1,5 @@
-import { check } from "k6";
+import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
+import { check, sleep } from "k6";
 import { vu } from "k6/execution";
 import http from "k6/http";
 
