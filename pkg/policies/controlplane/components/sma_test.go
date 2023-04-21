03e81946e10010302a3ee60562a007d097a9305b
Hasit Mistry <hasit@fluxninja.com>
2023-04-21 11:51:47 -0700

    Add test case for when an invalid signal is received in the middle

--- pkg/policies/controlplane/components/sma_test.go
+++ pkg/policies/controlplane/components/sma_test.go
@@ -116,6 +116,33 @@ var _ = Describe("SMA component", func() {
 		))
 	})
 
+	It("computes simple moving average with sma_window:5s, valid_during_warmup:true", func() {
+		circuit, err := sim.NewCircuitFromYaml(
+			`
+			evaluation_interval: 1s
+			components:
+			- sma:
+				parameters:
+					sma_window: 5s
+					valid_during_warmup: true
+				in_ports:
+					input: { signal_name: INPUT }
+				out_ports:
+					output: { signal_name: SMA }
+			`,
+			sim.Inputs{
+				"INPUT": sim.NewInput([]float64{nan, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, nan, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0}),
+			},
+			sim.OutputSignals{"SMA"},
+		)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(circuit.RunDrainInputs()).To(Equal(
+			sim.Outputs{
+				"SMA": sim.NewReadings([]float64{nan, 0.0, 0.5, 1, 1.5, 2.0, 3.0, 4.0, 4.0, 3.6, 3.2, 2.8, 2.4, 2.0, 3.0, 4.0}),
+			},
+		))
+	})
+
 	It("computes simple moving average with evaluation_interval:0.5s, sma_window:5s, valid_during_warmup:true", func() {
 		circuit, err := sim.NewCircuitFromYaml(
 			`
