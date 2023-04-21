c8dd9ae04b78668860f520ba2c590d24131f34d1
Hasit Mistry <hasit@fluxninja.com>
2023-04-21 12:00:10 -0700

    Add a test case for when invalid signals count reaches window size and should essentially reset the state

--- pkg/policies/controlplane/components/sma_test.go
+++ pkg/policies/controlplane/components/sma_test.go
@@ -116,7 +116,7 @@ var _ = Describe("SMA component", func() {
 		))
 	})
 
-	It("computes simple moving average with sma_window:5s, valid_during_warmup:true", func() {
+	It("computes simple moving average with sma_window:5s, valid_during_warmup:true and 1 invalid signal in the middle", func() {
 		circuit, err := sim.NewCircuitFromYaml(
 			`
 			evaluation_interval: 1s
@@ -139,6 +139,33 @@ var _ = Describe("SMA component", func() {
 		Expect(circuit.RunDrainInputs()).To(Equal(
 			sim.Outputs{
 				"SMA": sim.NewReadings([]float64{nan, 0.0, 0.5, 1, 1.5, 2.0, 3.0, 4.0, 4.0, 3.6, 3.2, 2.8, 2.4, 2.0, 3.0, 4.0}),
+			},
+		))
+	})
+
+	It("computes simple moving average with sma_window:5s, valid_during_warmup:true and 5 invalid signal in total", func() {
+		circuit, err := sim.NewCircuitFromYaml(
+			`
+			evaluation_interval: 1s
+			components:
+			- sma:
+				parameters:
+					sma_window: 5s
+					valid_during_warmup: true
+				in_ports:
+					input: { signal_name: INPUT }
+				out_ports:
+					output: { signal_name: SMA }
+			`,
+			sim.Inputs{
+				"INPUT": sim.NewInput([]float64{nan, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, nan, nan, nan, nan, nan, 1.0, 5.0, 6.0}),
+			},
+			sim.OutputSignals{"SMA"},
+		)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(circuit.RunDrainInputs()).To(Equal(
+			sim.Outputs{
+				"SMA": sim.NewReadings([]float64{nan, 0.0, 0.5, 1, 1.5, 2.0, 3.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 1, 3.0, 4.0}),
 			},
 		))
 	})
