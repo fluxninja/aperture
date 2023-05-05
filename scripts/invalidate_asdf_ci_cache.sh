632ae48ffc22e5cbb618fe979b1dd8699e5a1664
Harjot Gill <harjot.gill@fluxninja.com>
2023-05-05 16:20:10 -0700

    circleci: move cache paths around

--- scripts/invalidate_asdf_ci_cache.sh
+++ scripts/invalidate_asdf_ci_cache.sh
@@ -13,8 +13,8 @@ fi
 	"$AWK" -i inplace '/aperture-asdf-cache-v[0-9]+-/ {match($0, /aperture-asdf-cache-v([0-9]+)(.*)/, a); $0 = gensub(/aperture-asdf-cache-v[0-9]+-/, "aperture-asdf-cache-v" a[1]+1 "-", "g", $0)} {print}' "$file"
 done
 
-# invalidate aperture-v<version>-go-cache-* as well
+# invalidate aperture-v<version>-daily-cache-* as well
 # shellcheck disable=SC2016
 "$FIND" .circleci -type f \( -name "*.yml" -o -name "*.yaml" \) -print0 | while read -r -d $'\0' file; do
-	"$AWK" -i inplace '/aperture-v[0-9]+-go-cache-/ {match($0, /aperture-v([0-9]+)(.*)/, a); $0 = gensub(/aperture-v[0-9]+-go-cache-/, "aperture-v" a[1]+1 "-go-cache-", "g", $0)} {print}' "$file"
+	"$AWK" -i inplace '/aperture-v[0-9]+-daily-cache-/ {match($0, /aperture-v([0-9]+)(.*)/, a); $0 = gensub(/aperture-v[0-9]+-daily-cache-/, "aperture-v" a[1]+1 "-daily-cache-", "g", $0)} {print}' "$file"
 done
