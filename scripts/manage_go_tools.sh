337d0a35ba7085960cae2ebe4a0cd077c21ce41c
Harjot Gill <harjot.gill@fluxninja.com>
2023-05-05 13:07:01 -0700

    reshim after installing tools

--- scripts/manage_go_tools.sh
+++ scripts/manage_go_tools.sh
@@ -5,23 +5,20 @@ set -euo pipefail
 git_root="$(git rev-parse --show-toplevel)"
 readonly git_root="${git_root}"
 
-install_go_tools() {
-	printf 'Installing Go tools\n'
-	go env
-	# install go tools
-	pushd "${git_root}" >/dev/null
-	make install-go-tools
-	popd >/dev/null
-}
+pushd "${git_root}" >/dev/null
 
 if ! command -v go &>/dev/null; then
 	printf 'Installing Go\n'
-	pushd "${git_root}" >/dev/null
 	./scripts/manage_asdf_tools.sh setup golang
-	if asdf where golang &>/dev/null; then
-		asdf reshim golang
-	fi
-	popd >/dev/null
 fi
 
-install_go_tools
+printf 'Installing Go tools\n'
+go env
+# install go tools
+make install-go-tools
+
+if asdf where golang &>/dev/null; then
+	asdf reshim golang
+fi
+
+popd >/dev/null
--- scripts/manage_python_tools.sh
+++ scripts/manage_python_tools.sh
@@ -5,22 +5,18 @@ set -euo pipefail
 git_root="$(git rev-parse --show-toplevel)"
 readonly git_root="${git_root}"
 
-install_python_tools() {
-printf 'Installing Python tools\n'
 pushd "${git_root}" >/dev/null
-make install-python-tools
-popd >/dev/null
-}
 
 if ! command -v python &>/dev/null; then
-  printf 'Installing Python\n'
-  pushd "${git_root}" >/dev/null
-  ./scripts/manage_asdf_tools.sh setup python
-  popd >/dev/null
-
-  if asdf where python &>/dev/null; then
-		asdf reshim python
-	fi
+	printf 'Installing Python\n'
+	./scripts/manage_asdf_tools.sh setup python
 fi
 
-install_python_tools
+printf 'Installing Python tools\n'
+make install-python-tools
+
+if asdf where python &>/dev/null; then
+	asdf reshim python
+fi
+
+popd >/dev/null
