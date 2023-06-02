1d305319aad44081cb35f516e2482948f0d83386
Filip Chmielewski <filip.chmielewski@fluxninja.com>
2023-06-02 06:28:06 +0200

    Java SDK: Add missing javadoc comments

--- sdks/aperture-java/lib/core/src/main/java/com/fluxninja/aperture/sdk/FlowDecision.java
+++ sdks/aperture-java/lib/core/src/main/java/com/fluxninja/aperture/sdk/FlowDecision.java
@@ -1,5 +1,9 @@
 package com.fluxninja.aperture.sdk;
 
+/**
+ * Represents a decision made by Aperture Agent on a {@link Flow} or {@link TrafficFlow}, or a lack
+ * thereof.
+ */
 public enum FlowDecision {
     Accepted,
     Rejected,
