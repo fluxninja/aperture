19b0dbd53677aa1dba76390fc5f1edf4848d5f5e
Hardik Shingala <hardik.shingala@fluxninja.com>
2023-06-08 12:10:51 +0530

    Updated documentation

--- gateways/lua/kong/handler.lua
+++ gateways/lua/kong/handler.lua
@@ -3,15 +3,15 @@ local log = require "kong.plugins.aperture-plugin.log"
 local headers = require "kong.plugins.aperture-plugin.headers"
 
 local ApertureHandler = {
-  VERSION  = "0.1.0",
-  PRIORITY = 10000,
+    VERSION = "0.1.0",
+    PRIORITY = 10000
 }
 
 function ApertureHandler:access(config)
-  local authorized_status = access(config.control_point)
-  if authorized_status ~= ngx.HTTP_OK then
-    return ngx.exit(authorized_status)
-  end
+    local authorized_status = access(config.control_point)
+    if authorized_status ~= ngx.HTTP_OK then
+        return ngx.exit(authorized_status)
+    end
 end
 
 ApertureHandler.log = log
