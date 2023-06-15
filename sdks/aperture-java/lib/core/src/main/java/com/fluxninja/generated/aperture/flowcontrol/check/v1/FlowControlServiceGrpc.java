2e0472230a38b868db80c18caba0f822b2a85f9f
Hardik Shingala <34568645+hdkshingala@users.noreply.github.com>
2023-06-15 19:13:27 +0530

    Fixed pre-commit on main (#2161)

--- sdks/aperture-java/lib/core/src/main/java/com/fluxninja/generated/aperture/flowcontrol/check/v1/FlowControlServiceGrpc.java
+++ sdks/aperture-java/lib/core/src/main/java/com/fluxninja/generated/aperture/flowcontrol/check/v1/FlowControlServiceGrpc.java
@@ -8,7 +8,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.55.1)",
+    value = "by gRPC proto compiler (version 1.56.0)",
     comments = "Source: aperture/flowcontrol/check/v1/check.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class FlowControlServiceGrpc {
