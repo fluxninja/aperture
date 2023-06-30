e4007377f138d45c6b36f13214f0f47688ac2b48
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2023-06-30 14:07:06 +0200

    Better fx error handling

--- cmd/aperture-agent/agent.go
+++ cmd/aperture-agent/agent.go
@@ -59,12 +59,15 @@ func main() {
 		otelcollector.Module(),
 	)
 
+	defer log.WaitFlush()
+
 	if err := app.Err(); err != nil {
 		visualize, viserr := fx.VisualizeError(err)
 		if viserr != nil {
-			log.Panic().Err(viserr).Msgf("Failed to visualize fx error: %s", viserr)
+			log.Error().Err(viserr).Msgf("Failed to visualize fx error: %s", viserr)
 		}
-		log.Panic().Err(err).Msg("fx.New failed: " + visualize)
+		log.Error().Err(err).Msg("fx.New failed: " + visualize)
+		return
 	}
 
 	log.Info().Msg("aperture-agent app created")
