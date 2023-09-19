74d17e9de4a70e855e62fdecd857af52819423ad
Hasit Mistry <hasit@fluxninja.com>
2023-09-19 10:39:28 -0700

    Update traffic pattern in graceful-js scenario

--- playground/scenarios/graceful-js/load-generator/test.js
+++ playground/scenarios/graceful-js/load-generator/test.js
@@ -5,9 +5,9 @@ import http from "k6/http";
 
 export let vuStages = [
   { duration: "1s", target: 5 },
-  { duration: "2m", target: 5 },
-  { duration: "1s", target: 100 },
-  { duration: "5m", target: 100 },
+  { duration: "1m", target: 5 },
+  { duration: "1s", target: 50 },
+  { duration: "5m", target: 50 },
 ];
 
 export let options = {
