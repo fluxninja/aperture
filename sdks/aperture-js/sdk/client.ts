aa68a284024b02675092b1c54d28e4c269dc7d2a
Harjot Gill <harjot.gill@fluxninja.com>
2023-09-19 18:27:24 -0700

    aperture-js: set additional grpc options (#2649)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ### Summary by CodeRabbit
    
    - Refactor: Removed the `grpc.client_idle_timeout_ms` configuration
    option from the `ApertureClient` class in
    `sdks/aperture-js/sdk/client.ts`. This change simplifies the client
    configuration and improves maintainability. Please note that this may
    affect how your application handles idle gRPC clients.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- sdks/aperture-js/sdk/client.ts
+++ sdks/aperture-js/sdk/client.ts
@@ -34,7 +34,6 @@ export class ApertureClient {
       "grpc.keepalive_time_ms": 10000,
       "grpc.keepalive_timeout_ms": 5000,
       "grpc.keepalive_permit_without_calls": 1,
-      "grpc.client_idle_timeout_ms": 0,
     });
 
     this.exporter = new OTLPTraceExporter({
