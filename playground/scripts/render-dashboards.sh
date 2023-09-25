92eaa25eccde4ae6ac56d2d41c735884bbd1492e
Hasit Mistry <hasit@fluxninja.com>
2023-09-25 16:30:11 -0700

    Fix rendering infra dashboards in Tilt
    - Remove dashboards mixin intermediate step
    - Simply dashboards generation

---
+++ playground/scripts/render-dashboards.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+aperturectl=$1
+blueprints_uri=$2
+blueprint_name=$3
+values_file=$4
+
+# delete the temp dir
+_GEN_DIR="/tmp/aperture/_gen"
+rm -rf -- "$_GEN_DIR"
+
+"${aperturectl}" blueprints generate --name "${blueprint_name}" --uri "${blueprints_uri}" \
+	--values-file "${values_file}" --output-dir "${_GEN_DIR}" >&2
+
+dashboard_dir="${_GEN_DIR}/dashboards"
+# check if the dashboard dir exists
+if [ ! -d "${dashboard_dir}" ]; then
+	echo >&2 "Could not find dashboard directory: ${dashboard_dir}"
+	exit 1
+fi
+
+# get the list of dashboards as a absolute path
+dashboards=$(find "${dashboard_dir}" -type f -name '*.json' -print0 | xargs -0)
+
+echo "${dashboards}"
