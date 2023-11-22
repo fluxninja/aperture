8ad5437d1c7c91594b677f2c1dcdd32b49b24240
Hardik Shingala <hardik.shingala@fluxninja.com>
2023-11-22 17:07:16 +0530

    Fix pod labels and annotations reconciliation in operator

--- operator/controllers/agent/daemonset_test.go
+++ operator/controllers/agent/daemonset_test.go
@@ -17,6 +17,8 @@ limitations under the License.
 package agent
 
 import (
+	"fmt"
+
 	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -664,6 +666,10 @@ var _ = Describe("Test DaemonSet Mutate", func() {
 		err := daemonsetMutate(dms, expected.Spec)()
 
 		Expect(err).NotTo(HaveOccurred())
+		fmt.Println(dms.Spec.Template.Annotations)
+		fmt.Println(expected.Spec.Template.Annotations)
+		fmt.Println(dms.Spec.Template.Labels)
+		fmt.Println(expected.Spec.Template.Labels)
 		Expect(dms).To(Equal(expected))
 	})
 })
