581491009c4d01d49f895803fccb2f027b3420a2
Hasit Mistry <hasit@fluxninja.com>
2023-12-06 20:54:49 -0800

    Refactor code by removing unused properties and dependencies (#3036)
    ### Description of change
    
    ##### Checklist
    
    - [ ] Tested in playground or other setup
    - [ ] Screenshot (Grafana) from playground added to PR for 15+ minute
    run
    - [ ] Documentation is changed or added
    - [ ] Tests and/or benchmarks are included
    - [ ] Breaking changes
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Documentation**
    - Updated the configuration specification to reflect changes in rate
    limiting features.
    
    - **Refactor**
    - Removed the `adaptive_load_scheduler` from the `RateLimiter`
    configuration to streamline rate limiting policies.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- api/gen/proto/go/aperture/policy/language/v1/flowcontrol_vtproto.pb.go
+++ api/gen/proto/go/aperture/policy/language/v1/flowcontrol_vtproto.pb.go
@@ -542,16 +542,6 @@ func (m *RateLimiter_Parameters) MarshalToSizedBufferVT(dAtA []byte) (int, error
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
-	if m.AdaptiveLoadScheduler != nil {
-		size, err := m.AdaptiveLoadScheduler.MarshalToSizedBufferVT(dAtA[:i])
-		if err != nil {
-			return 0, err
-		}
-		i -= size
-		i = encodeVarint(dAtA, i, uint64(size))
-		i--
-		dAtA[i] = 0x42
-	}
 	if m.DelayInitialFill {
 		i--
 		if m.DelayInitialFill {
@@ -4256,10 +4246,6 @@ func (m *RateLimiter_Parameters) SizeVT() (n int) {
 	if m.DelayInitialFill {
 		n += 2
 	}
-	if m.AdaptiveLoadScheduler != nil {
-		l = m.AdaptiveLoadScheduler.SizeVT()
-		n += 1 + l + sov(uint64(l))
-	}
 	n += len(m.unknownFields)
 	return n
 }
@@ -6896,42 +6882,6 @@ func (m *RateLimiter_Parameters) UnmarshalVT(dAtA []byte) error {
 				}
 			}
 			m.DelayInitialFill = bool(v != 0)
-		case 8:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field AdaptiveLoadScheduler", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflow
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLength
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLength
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if m.AdaptiveLoadScheduler == nil {
-				m.AdaptiveLoadScheduler = &AdaptiveLoadScheduler{}
-			}
-			if err := m.AdaptiveLoadScheduler.UnmarshalVT(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skip(dAtA[iNdEx:])
