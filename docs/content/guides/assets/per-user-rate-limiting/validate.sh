7bbfebe93c7243fe70acad485201fcd6162043f6
Karanbir Singh <65090469+karansohi@users.noreply.github.com>
2023-12-06 22:33:15 -0800

    Add Executable Examples in Typescript SDK (#3032)
    ### Description of change
    
    ##### Checklist
    
    - [ ] Tested in playground or other setup
    - [ ] Screenshot (Grafana) from playground added to PR for 15+ minute
    run
    - [x] Documentation is changed or added
    - [ ] Tests and/or benchmarks are included
    - [ ] Breaking changes
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Documentation**
    - Updated the guide on per-user rate limiting with new file paths for
    policy and graph files.
    - Enhanced the documentation for using the Aperture SDK, including setup
    instructions and policy creation steps.
    - Added detailed steps for creating and monitoring rate limiting
    policies in the Aperture Cloud UI.
    
    - **New Features**
    - Introduced a quota scheduler example with functions for initializing
    the Aperture client and scheduling requests based on user tiers.
    - Added a rate limit example that demonstrates how to handle request
    rate limits using the Aperture client.
    
    - **Bug Fixes**
    - Corrected file paths in the validation script to align with the new
    naming conventions for generated files.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- docs/content/guides/assets/per-user-rate-limiting/validate.sh
+++ docs/content/guides/assets/per-user-rate-limiting/validate.sh
@@ -12,8 +12,8 @@ generate_from_values \
 	tmp
 
 # copy the generated policy and graph to this (assets) directory so that they can be used in the docs
-cp tmp/policies/static-rate-limiting-cr.yaml policy.yaml
-cp tmp/graphs/static-rate-limiting-cr.mmd graph.mmd
+cp tmp/policies/rate-limit-test-cr.yaml policy.yaml
+cp tmp/graphs/rate-limit-test-cr.mmd graph.mmd
 
 # git add the generated policy and graph
 "$git_root"/scripts/git_add_safely.sh policy.yaml graph.mmd
