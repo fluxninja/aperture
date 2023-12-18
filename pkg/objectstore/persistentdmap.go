8b37e79d2b97957367c757b9d5f8f002fb081a49
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2023-12-18 18:15:03 +0100

    Use pointers for ObjectStoreBackedDMap

--- pkg/objectstore/persistentdmap.go
+++ pkg/objectstore/persistentdmap.go
@@ -28,11 +28,11 @@ type ObjectStoreBackedDMap struct {
 	backingStorage ObjectStorageIface
 }
 
-func (o ObjectStoreBackedDMap) generateObjectKey(key string) string {
+func (o *ObjectStoreBackedDMap) generateObjectKey(key string) string {
 	return fmt.Sprintf("%s-%s-%s", o.backingStorage.KeyPrefix(), o.dmap.Name(), key)
 }
 
-func (o ObjectStoreBackedDMap) Delete(ctx context.Context, keys ...string) (int, error) {
+func (o *ObjectStoreBackedDMap) Delete(ctx context.Context, keys ...string) (int, error) {
 	for _, key := range keys {
 		err := o.backingStorage.Delete(ctx, o.generateObjectKey(key))
 		if err != nil {
@@ -43,11 +43,11 @@ func (o ObjectStoreBackedDMap) Delete(ctx context.Context, keys ...string) (int,
 	return o.dmap.Delete(ctx, keys...)
 }
 
-func (o ObjectStoreBackedDMap) Destroy(ctx context.Context) error {
+func (o *ObjectStoreBackedDMap) Destroy(ctx context.Context) error {
 	return o.dmap.Destroy(ctx)
 }
 
-func (o ObjectStoreBackedDMap) Expire(ctx context.Context, key string, timeout time.Duration) error {
+func (o *ObjectStoreBackedDMap) Expire(ctx context.Context, key string, timeout time.Duration) error {
 	err := o.backingStorage.Delete(ctx, o.generateObjectKey(key))
 	if err != nil {
 		return err
@@ -56,11 +56,11 @@ func (o ObjectStoreBackedDMap) Expire(ctx context.Context, key string, timeout t
 	return o.dmap.Expire(ctx, key, timeout)
 }
 
-func (o ObjectStoreBackedDMap) Function(ctx context.Context, label string, function string, arg []byte) ([]byte, error) {
+func (o *ObjectStoreBackedDMap) Function(ctx context.Context, label string, function string, arg []byte) ([]byte, error) {
 	return o.dmap.Function(ctx, label, function, arg)
 }
 
-func (o ObjectStoreBackedDMap) Get(ctx context.Context, key string) (*olric.GetResponse, error) {
+func (o *ObjectStoreBackedDMap) Get(ctx context.Context, key string) (*olric.GetResponse, error) {
 	resp, err := o.dmap.Get(ctx, key)
 	if err != nil {
 		if errors.Is(err, olric.ErrKeyNotFound) {
@@ -85,19 +85,19 @@ func (o ObjectStoreBackedDMap) Get(ctx context.Context, key string) (*olric.GetR
 	return resp, nil
 }
 
-func (o ObjectStoreBackedDMap) Lock(ctx context.Context, key string, deadline time.Duration) (olric.LockContext, error) {
+func (o *ObjectStoreBackedDMap) Lock(ctx context.Context, key string, deadline time.Duration) (olric.LockContext, error) {
 	return o.Lock(ctx, key, deadline)
 }
 
-func (o ObjectStoreBackedDMap) LockWithTimeout(ctx context.Context, key string, timeout time.Duration, deadline time.Duration) (olric.LockContext, error) {
+func (o *ObjectStoreBackedDMap) LockWithTimeout(ctx context.Context, key string, timeout time.Duration, deadline time.Duration) (olric.LockContext, error) {
 	return o.LockWithTimeout(ctx, key, timeout, deadline)
 }
 
-func (o ObjectStoreBackedDMap) Name() string {
+func (o *ObjectStoreBackedDMap) Name() string {
 	return o.dmap.Name()
 }
 
-func (o ObjectStoreBackedDMap) Put(ctx context.Context, key string, value interface{}, options ...olric.PutOption) error {
+func (o *ObjectStoreBackedDMap) Put(ctx context.Context, key string, value interface{}, options ...olric.PutOption) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		log.Error().Msg("Object storage backed cache only supports []byte values")
@@ -113,11 +113,11 @@ func (o ObjectStoreBackedDMap) Put(ctx context.Context, key string, value interf
 	return o.dmap.Put(ctx, key, value, options...)
 }
 
-func NewPersistentDMap(dmap olric.DMap, backingStorage ObjectStorageIface) ObjectStoreBackedDMap {
-	return ObjectStoreBackedDMap{
+func NewPersistentDMap(dmap olric.DMap, backingStorage ObjectStorageIface) *ObjectStoreBackedDMap {
+	return &ObjectStoreBackedDMap{
 		dmap:           dmap,
 		backingStorage: backingStorage,
 	}
 }
 
-var _ olric.DMap = ObjectStoreBackedDMap{}
+var _ olric.DMap = &ObjectStoreBackedDMap{}
