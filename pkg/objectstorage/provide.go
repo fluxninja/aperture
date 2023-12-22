b4d0c61b9da055901079df228e9df61560e9e091
Krzysztof Klimonda <krzysztof.klimonda@fluxninja.com>
2023-12-22 10:58:50 +0100

    Add agent group to the object name prefix

--- pkg/objectstorage/provide.go
+++ pkg/objectstorage/provide.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	agentinfo "github.com/fluxninja/aperture/v2/pkg/agent-info"
+
 	"cloud.google.com/go/storage"
 
 	"github.com/fluxninja/aperture/v2/pkg/config"
@@ -16,6 +18,7 @@ import (
 type ProvideParams struct {
 	fx.In
 
+	AgentInfo    *agentinfo.AgentInfo
 	Unmarshaller config.Unmarshaller
 }
 
@@ -44,9 +47,11 @@ func Provide(in ProvideParams) (*ObjectStorage, error) {
 	}
 	bucket := client.Bucket(cfg.Bucket)
 
+	keyPrefix := fmt.Sprintf("%s-%s", cfg.KeyPrefix, in.AgentInfo.GetAgentGroup())
+
 	objStorage := &ObjectStorage{
 		bucket:     bucket,
-		keyPrefix:  cfg.KeyPrefix,
+		keyPrefix:  keyPrefix,
 		operations: make(chan *Operation),
 	}
 
